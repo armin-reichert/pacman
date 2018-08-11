4a74b257b35515d4f8c20f49016da47166998230
armin <armin@X230.fritz.box>
2018-08-11 07:59:14 +0200

    cleanup

--- src/de/amr/games/pacman/model/Content.java
+++ src/de/amr/games/pacman/model/Content.java
@@ -3,21 +3,21 @@ package de.amr.games.pacman.model;
 public interface Content {
 
 	// Structure
-	public static final char EMPTY = ' ';
-	public static final char WALL = '#';
-	public static final char DOOR = 'D';
-	public static final char TUNNEL = 'T';
+	static final char EMPTY = ' ';
+	static final char WALL = '#';
+	static final char DOOR = 'D';
+	static final char TUNNEL = 'T';
 
 	// Position markers
-	public static final char POS_BONUS = '$';
-	public static final char POS_PACMAN = 'O';
-	public static final char POS_BLINKY = 'B';
-	public static final char POS_INKY = 'I';
-	public static final char POS_PINKY = 'P';
-	public static final char POS_CLYDE = 'C';
+	static final char POS_BONUS = '$';
+	static final char POS_PACMAN = 'O';
+	static final char POS_BLINKY = 'B';
+	static final char POS_INKY = 'I';
+	static final char POS_PINKY = 'P';
+	static final char POS_CLYDE = 'C';
 
 	// Food
-	public static final char PELLET = '.';
-	public static final char ENERGIZER = '*';
-	public static final char EATEN = ':';
+	static final char PELLET = '.';
+	static final char ENERGIZER = '*';
+	static final char EATEN = ':';
 }
