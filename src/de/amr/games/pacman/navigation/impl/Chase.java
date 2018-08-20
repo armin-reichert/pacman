cd0003c7632efde3e74b34a69a2ea77c07596f8d
Armin Reichert <armin.reichert@web.de>
2018-08-20 19:06:25 +0200

    simplified

--- src/de/amr/games/pacman/navigation/impl/Chase.java
+++ src/de/amr/games/pacman/navigation/impl/Chase.java
@@ -1,32 +1,13 @@
 package de.amr.games.pacman.navigation.impl;
 
 import de.amr.games.pacman.actor.core.MazeMover;
-import de.amr.games.pacman.model.Maze;
-import de.amr.games.pacman.navigation.MazeRoute;
-import de.amr.games.pacman.navigation.Navigation;
 
 /**
  * Chasing a victim through the maze.
  */
-class Chase implements Navigation {
-
-	private final MazeMover victim;
+class Chase extends FollowTargetTile {
 
 	public Chase(MazeMover victim) {
-		this.victim = victim;
-	}
-
-	@Override
-	public MazeRoute computeRoute(MazeMover chaser) {
-		MazeRoute route = new MazeRoute();
-		Maze maze = chaser.getMaze();
-		if (victim.inTeleportSpace()) {
-			// chaser cannot see victim
-			route.dir = chaser.getNextDir();
-		} else {
-			route.path = maze.findPath(chaser.getTile(), victim.getTile());
-			route.dir = maze.alongPath(route.path).orElse(chaser.getNextDir());
-		}
-		return route;
+		super(victim.getMaze(), victim::getTile);
 	}
 }
