40c72fd440b1f7667ccbb665cbb24d0d498b24bc
Armin Reichert <armin.reichert@web.de>
2018-08-27 20:09:19 +0200

    fix

--- src/de/amr/games/pacman/view/core/BlinkingText.java
+++ src/de/amr/games/pacman/view/core/BlinkingText.java
@@ -34,7 +34,7 @@ public class BlinkingText extends GameEntity implements ViewAnimation {
 		this.color = Color.YELLOW;
 		createSprite();
 	}
-	
+
 	public BlinkingText set(String text, Font font, Color background, Color color) {
 		Objects.requireNonNull(text);
 		Objects.requireNonNull(font);
@@ -47,31 +47,31 @@ public class BlinkingText extends GameEntity implements ViewAnimation {
 		createSprite();
 		return this;
 	}
-	
+
 	public void setText(String text) {
 		Objects.requireNonNull(text);
 		this.text = text;
 		createSprite();
 	}
-	
+
 	public void setFont(Font font) {
 		Objects.requireNonNull(font);
 		this.font = font;
 		createSprite();
 	}
-	
+
 	public void setColor(Color color) {
 		Objects.requireNonNull(color);
 		this.color = color;
 		createSprite();
 	}
-	
+
 	public void setBackground(Color background) {
 		Objects.requireNonNull(background);
 		this.background = background;
 		createSprite();
 	}
-	
+
 	public void setBlinkTime(int millis) {
 		sprite.animate(AnimationType.BACK_AND_FORTH, millis);
 	}
@@ -81,7 +81,11 @@ public class BlinkingText extends GameEntity implements ViewAnimation {
 		BufferedImage image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
 		Graphics2D g = image.createGraphics();
 		g.setFont(font);
-		width = g.getFontMetrics().stringWidth(text);
+		String patchedText = text;
+		if (font.getFontName().equals("ArcadeClassic")) {
+			patchedText = text.replace(" ", "    ");
+		}
+		width = g.getFontMetrics().stringWidth(patchedText);
 		width = width == 0 ? 1 : width;
 		height = font.getSize();
 		g.dispose();
@@ -92,15 +96,11 @@ public class BlinkingText extends GameEntity implements ViewAnimation {
 		g.fillRect(0, 0, width, height);
 		g.setColor(color);
 		g.setFont(font);
-		if (font.getFontName().equals("ArcadeClassic")) {
-			g.drawString(text.replace(" ", "    "), 0, height);
-		} else {
-			g.drawString(text, 0, height);
-		}
+		g.drawString(patchedText, 0, height);
 		g.dispose();
-		sprite = new Sprite(image, null).animate(AnimationType.BACK_AND_FORTH, 750);
+		sprite = new Sprite(image, null).animate(AnimationType.BACK_AND_FORTH, 500 );
 	}
-	
+
 	@Override
 	public int getWidth() {
 		return width;
@@ -110,7 +110,7 @@ public class BlinkingText extends GameEntity implements ViewAnimation {
 	public int getHeight() {
 		return height;
 	}
-	
+
 	@Override
 	public boolean isAnimationCompleted() {
 		return false;
@@ -125,7 +125,7 @@ public class BlinkingText extends GameEntity implements ViewAnimation {
 	public Stream<Sprite> getSprites() {
 		return Stream.of(sprite);
 	}
-	
+
 	@Override
 	public void draw(Graphics2D g) {
 		g.setColor(background);
