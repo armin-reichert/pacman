4c8602ba8012494c36db80bf56a755b0faf327d0
Armin Reichert <armin.reichert@web.de>
2018-09-11 14:43:15 +0200

    cleanup

--- src/de/amr/games/pacman/navigation/Navigation.java
+++ src/de/amr/games/pacman/navigation/Navigation.java
@@ -2,10 +2,33 @@ package de.amr.games.pacman.navigation;
 
 import de.amr.games.pacman.actor.Actor;
 
+/**
+ * Navigation behavior of actors.
+ * 
+ * @author Armin Reichert
+ *
+ * @param <T>
+ *          actor type
+ */
 public interface Navigation<T extends Actor> {
 
-	MazeRoute computeRoute(T mover);
+	/**
+	 * Computes the route the actor should take when this method is called.
+	 * 
+	 * @param actor
+	 *                the moving actor
+	 * @return the route the actor should take
+	 */
+	MazeRoute computeRoute(T actor);
 
-	default void computeStaticPath(T mover) {
+	/**
+	 * Triggers computation of a static path for the actor. The concrete implementation can then store
+	 * this path until the actor has reached the target or another decision has to be taken. Using a
+	 * static path can save lots of path finder calls.
+	 * 
+	 * @param actor
+	 *                the moving actor
+	 */
+	default void computeStaticPath(T actor) {
 	}
 }
