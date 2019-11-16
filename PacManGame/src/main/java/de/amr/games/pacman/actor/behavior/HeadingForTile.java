424016f216b8ab5218f81a8a98ab7124e690f55a
Armin Reichert <armin.reichert@web.de>
2019-11-16 16:33:36 +0100

    IntStream

--- PacManGame/src/main/java/de/amr/games/pacman/actor/behavior/HeadingForTile.java
+++ PacManGame/src/main/java/de/amr/games/pacman/actor/behavior/HeadingForTile.java
@@ -5,7 +5,7 @@ import static de.amr.games.pacman.model.Maze.NESW;
 import java.util.Collections;
 import java.util.Objects;
 import java.util.function.Supplier;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 import de.amr.games.pacman.actor.MazeMover;
 import de.amr.games.pacman.model.Maze;
@@ -17,18 +17,16 @@ import de.amr.graph.grid.impl.Top4;
  * "http://gameinternals.com/post/2072558330/understanding-pac-man-ghost-behavior">here</a>:
  *
  * <p>
- * The next step is understanding exactly how the ghosts attempt to reach their
- * target tiles. The ghosts’ AI is very simple and short-sighted, which makes
- * the complex behavior of the ghosts even more impressive. Ghosts only ever
- * plan one step into the future as they move about the maze. <br/>
- * Whenever a ghost enters a new tile, it looks ahead to the next tile that it
- * will reach, and makes a decision about which direction it will turn when it
- * gets there. These decisions have one very important restriction, which is
- * that ghosts may never choose to reverse their direction of travel. That is, a
- * ghost cannot enter a tile from the left side and then decide to reverse
- * direction and move back to the left. The implication of this restriction is
- * that whenever a ghost enters a tile with only two exits, it will always
- * continue in the same direction. </cite>
+ * The next step is understanding exactly how the ghosts attempt to reach their target tiles. The
+ * ghosts’ AI is very simple and short-sighted, which makes the complex behavior of the ghosts even
+ * more impressive. Ghosts only ever plan one step into the future as they move about the maze.
+ * <br/>
+ * Whenever a ghost enters a new tile, it looks ahead to the next tile that it will reach, and makes
+ * a decision about which direction it will turn when it gets there. These decisions have one very
+ * important restriction, which is that ghosts may never choose to reverse their direction of
+ * travel. That is, a ghost cannot enter a tile from the left side and then decide to reverse
+ * direction and move back to the left. The implication of this restriction is that whenever a ghost
+ * enters a tile with only two exits, it will always continue in the same direction. </cite>
  * </p>
  * 
  * @author Armin Reichert
@@ -67,14 +65,14 @@ class HeadingForTile implements Steering {
 		/* If a new tile is entered, decide where to go as described above. */
 		if (actor.enteredNewTile) {
 			// try directions in order: up > left > down > right
-			Stream.of(Top4.N, Top4.W, Top4.S, Top4.E)
+			IntStream.of(Top4.N, Top4.W, Top4.S, Top4.E)
 			/*@formatter:off*/
 				 // never reverse direction	
 				.filter(dir -> dir != NESW.inv(actor.moveDir))
 				 // cannot move upwards at blocked intersection
 				.filter(dir -> !(dir == Top4.N && maze.isNoUpIntersection(actorTile)))
 				 // check if neighbor tile to this direction is accessible
-				.map(dir -> maze.tileToDir(actorTile, dir))
+				.mapToObj(dir -> maze.tileToDir(actorTile, dir))
 				.filter(actor::canEnterTile)
 				 // select tile with smallest straight line distance to target
 				.sorted((t1, t2) -> Integer.compare(dist(t1, actor.targetTile), dist(t2, actor.targetTile)))
