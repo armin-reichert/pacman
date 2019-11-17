96fa937b0fab2c9777f4213f9f6dc72d2be3fee4
Armin Reichert <armin.reichert@web.de>
2019-11-17 09:45:54 +0100

    route color

--- PacManGame/src/main/java/de/amr/games/pacman/view/play/PlayViewXtended.java
+++ PacManGame/src/main/java/de/amr/games/pacman/view/play/PlayViewXtended.java
@@ -53,8 +53,7 @@ public class PlayViewXtended extends PlayView {
 	private static BufferedImage createGridImage(int numRows, int numCols) {
 		GraphicsConfiguration conf = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice()
 				.getDefaultConfiguration();
-		BufferedImage image = conf.createCompatibleImage(numCols * TS, numRows * TS + 1,
-				Transparency.TRANSLUCENT);
+		BufferedImage image = conf.createCompatibleImage(numCols * TS, numRows * TS + 1, Transparency.TRANSLUCENT);
 		Graphics2D g = image.createGraphics();
 		g.setColor(new Color(0, 60, 0));
 		for (int row = 0; row <= numRows; ++row) {
@@ -105,7 +104,7 @@ public class PlayViewXtended extends PlayView {
 		}
 		if (Keyboard.keyPressedOnce(KeyEvent.VK_R)) {
 			showRoutes = !showRoutes;
-		  game.ghosts().forEach(ghost -> ghost.visualizeCompletePath = showRoutes);
+			game.ghosts().forEach(ghost -> ghost.visualizeCompletePath = showRoutes);
 		}
 		if (Keyboard.keyPressedOnce(KeyEvent.VK_B)) {
 			toggleGhost(game.blinky);
@@ -169,8 +168,7 @@ public class PlayViewXtended extends PlayView {
 					? String.format("%s(%s,%d|%d)[->%s]", name, ghost.state().id(), ghost.state().getTicksRemaining(),
 							ghost.state().getDuration(), nextState)
 					: String.format("%s(%s,%s)[->%s]", name, ghost.state().id(), INFTY, nextState);
-		}
-		else {
+		} else {
 			return ghost.state().getDuration() != State.ENDLESS ? String.format("%s(%s,%d|%d)", name,
 					ghost.state().id(), ghost.state().getTicksRemaining(), ghost.state().getDuration())
 					: String.format("%s(%s,%s)", name, ghost.state().id(), INFTY);
@@ -217,13 +215,14 @@ public class PlayViewXtended extends PlayView {
 	}
 
 	private void drawRoute(Graphics2D g, Ghost ghost) {
-		g.setColor(ghostColor(ghost));
+		Color color = ghostColor(ghost);
+		g.setColor(new Color(color.getRed(), color.getGreen(), color.getBlue(), 140));
 		if (ghost.targetTile != null) {
 			// draw target tile indicator
-			g.drawLine((int) ghost.tf.getCenter().x, (int) ghost.tf.getCenter().y,
-					ghost.targetTile.col * TS + TS / 2, ghost.targetTile.row * TS + TS / 2);
+			g.drawLine((int) ghost.tf.getCenter().x, (int) ghost.tf.getCenter().y, ghost.targetTile.col * TS + TS / 2,
+					ghost.targetTile.row * TS + TS / 2);
 			g.translate(ghost.targetTile.col * TS, ghost.targetTile.row * TS);
-			g.fillRect(TS / 4, TS / 4, TS / 2, TS / 2);
+			g.drawRect(TS / 4, TS / 4, TS / 2, TS / 2);
 			g.translate(-ghost.targetTile.col * TS, -ghost.targetTile.row * TS);
 		}
 		if (ghost.targetPath.size() > 1) {
@@ -235,8 +234,7 @@ public class PlayViewXtended extends PlayView {
 				int v2 = v.row * TS + TS / 2;
 				g.drawLine(u1, u2, v1, v2);
 			}
-		}
-		else {
+		} else {
 			// draw direction indicator
 			if (ghost.nextDir != -1) {
 				Vector2f center = ghost.tf.getCenter();
