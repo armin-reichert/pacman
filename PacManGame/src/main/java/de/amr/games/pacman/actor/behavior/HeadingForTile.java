53fa6999b9d9a0b5854222fd285d63dcdaa3bb02
Armin Reichert <armin.reichert@web.de>
2019-11-19 06:55:29 +0100

    simplified again

--- PacManGame/src/main/java/de/amr/games/pacman/actor/behavior/HeadingForTile.java
+++ PacManGame/src/main/java/de/amr/games/pacman/actor/behavior/HeadingForTile.java
@@ -1,5 +1,6 @@
 package de.amr.games.pacman.actor.behavior;
 
+import static de.amr.games.pacman.model.Maze.NESW;
 import static de.amr.graph.grid.impl.Top4.E;
 import static de.amr.graph.grid.impl.Top4.N;
 import static de.amr.graph.grid.impl.Top4.S;
@@ -13,7 +14,6 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import de.amr.games.pacman.actor.MazeMover;
 import de.amr.games.pacman.model.Maze;
@@ -21,12 +21,13 @@ import de.amr.games.pacman.model.Tile;
 import de.amr.graph.grid.impl.Top4;
 
 /**
- * A behavior which steers an actor (ghost) towards a (possibly moving) target tile. Each time the
- * {@link #steer(MazeMover)} method is called, the target tile is recomputed. There is also some
- * special logic for entering and exiting the ghost house.
+ * A behavior which steers an actor (ghost) towards a (possibly moving) target
+ * tile. Each time the {@link #steer(MazeMover)} method is called, the target
+ * tile is recomputed. There is also some special logic for entering and exiting
+ * the ghost house.
  * 
- * The detailed behavior is described
- * <a href="http://gameinternals.com/understanding-pac-man-ghost-behavior">here</a>.
+ * The detailed behavior is described <a href=
+ * "http://gameinternals.com/understanding-pac-man-ghost-behavior">here</a>.
  * 
  * @author Armin Reichert
  */
@@ -35,12 +36,11 @@ class HeadingForTile implements Steering {
 	private final Supplier<Tile> fnTargetTile;
 
 	/**
-	 * Creates a behavior which lets an actor heading for the target tile supplied by the given
-	 * function.
+	 * Creates a behavior which lets an actor heading for the target tile supplied
+	 * by the given function.
 	 * 
-	 * @param fnTargetTile
-	 *                       function supplying the target tile whenever the {@link #steer(MazeMover)}
-	 *                       method is called
+	 * @param fnTargetTile function supplying the target tile whenever the
+	 *                     {@link #steer(MazeMover)} method is called
 	 */
 	public HeadingForTile(Supplier<Tile> fnTargetTile) {
 		this.fnTargetTile = fnTargetTile;
@@ -66,61 +66,47 @@ class HeadingForTile implements Steering {
 		actor.targetPath = actor.visualizePath ? computePath(actor) : Collections.emptyList();
 	}
 
+	/** Directions in order used to compute the next move direction */
+	private static final List<Integer> DIRS_IN_ORDER = Collections.unmodifiableList(Arrays.asList(N, W, S, E));
+
 	/**
-	 * Computes the next move direction as described
-	 * <a href="http://gameinternals.com/understanding-pac-man-ghost-behavior">here.</a>
+	 * Computes the next move direction as described <a href=
+	 * "http://gameinternals.com/understanding-pac-man-ghost-behavior">here.</a>
 	 * 
 	 * <p>
-	 * Note: We use separate parameters for the actor's move direction, tile and target tile instead of
-	 * the members of the actor itself because the {@link #computePath(MazeMover, Tile)} method uses
-	 * this method without actually placing the actor at each tile of the path.
+	 * Note: We use separate parameters for the actor's move direction, tile and
+	 * target tile instead of the members of the actor itself because the
+	 * {@link #computePath(MazeMover, Tile)} method uses this method without
+	 * actually placing the actor at each tile of the path.
 	 * 
-	 * @param actor
-	 *                      a actor (normally a ghost)
-	 * @param moveDir
-	 *                      the actor's current move direction
-	 * @param currentTile
-	 *                      the actor's current tile
-	 * @param targetTile
-	 *                      the actor's current target tile
+	 * @param actor       a actor (normally a ghost)
+	 * @param moveDir     the actor's current move direction
+	 * @param currentTile the actor's current tile
+	 * @param targetTile  the actor's current target tile
 	 */
 	private static int computeNextDir(MazeMover actor, int moveDir, Tile currentTile, Tile targetTile) {
 		/*@formatter:off*/
 		Maze maze = actor.maze;
-		List<Integer> dirs = Stream.of(N, W, S, E)
-				.filter(dir -> dir != Maze.NESW.inv(moveDir))
-				.filter(dir -> actor.canEnterTile(currentTile, maze.tileToDir(currentTile, dir)))
-				.collect(Collectors.toList());
+		return DIRS_IN_ORDER.stream()
+			.filter(dir -> dir != NESW.inv(moveDir))
+			.filter(dir -> actor.canEnterTile(currentTile, maze.tileToDir(currentTile, dir)))
+			.sorted((dir1, dir2) -> {
+				Tile nb1 = maze.tileToDir(currentTile, dir1), nb2 = maze.tileToDir(currentTile, dir2);
+				int cmpByDistance = Integer.compare(distance(nb1, targetTile), distance(nb2, targetTile));
+				return cmpByDistance != 0
+					? cmpByDistance
+					: Integer.compare(DIRS_IN_ORDER.indexOf(dir1), DIRS_IN_ORDER.indexOf(dir2));
+			})
+			.findFirst().orElseThrow(IllegalStateException::new);
 		/*@formatter:on*/
-		if (dirs.isEmpty()) {
-			throw new IllegalStateException("Could not determine next move direction");
-		}
-		if (dirs.size() > 1) {
-			dirs.sort((dir1, dir2) -> {
-				Tile tile1 = maze.tileToDir(currentTile, dir1), tile2 = maze.tileToDir(currentTile, dir2);
-				int comparedByDistance = Integer.compare(distance(tile1, targetTile), distance(tile2, targetTile));
-				if (comparedByDistance != 0) {
-					return comparedByDistance;
-				}
-				/*
-				 * If two or more potential choices are an equal distance from the target, the decision between them
-				 * is made in the order of up > left > down. A decision to exit right can never be made in a
-				 * situation where two tiles are equidistant to the target, since any other option has a higher
-				 * priority.
-				 */
-				List<Integer> dirOrder = Arrays.asList(Top4.N, Top4.W, Top4.S, Top4.E);
-				return Integer.compare(dirOrder.indexOf(dir1), dirOrder.indexOf(dir2));
-			});
-		}
-		return dirs.get(0);
 	}
 
 	/**
-	 * Computes the complete path the actor would traverse until it would reach the target tile, a cycle
-	 * would occur or the borders of the board would be reached.
+	 * Computes the complete path the actor would traverse until it would reach the
+	 * target tile, a cycle would occur or the borders of the board would be
+	 * reached.
 	 * 
-	 * @param actor
-	 *                actor for which the path is computed
+	 * @param actor actor for which the path is computed
 	 * @return the path the actor would use
 	 */
 	private static List<Tile> computePath(MazeMover actor) {
