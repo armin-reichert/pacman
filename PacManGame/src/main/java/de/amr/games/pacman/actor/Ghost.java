7938ae474ceb11aa1c13afa4c91dc7ed481dd66c
Armin Reichert <armin.reichert@web.de>
2019-11-30 11:32:40 +0100

    cleanup

--- PacManGame/src/main/java/de/amr/games/pacman/actor/Ghost.java
+++ PacManGame/src/main/java/de/amr/games/pacman/actor/Ghost.java
@@ -249,9 +249,6 @@ public class Ghost extends Actor<GhostState> {
 				.when(LOCKED).then(LEAVING_HOUSE)
 					.condition(this::unlocked)
 			
-//				.when(LEAVING_HOUSE).then(FRIGHTENED)
-//					.condition(() -> leftHouse() && game.pacMan.hasPower())
-
 				.when(LEAVING_HOUSE).then(SCATTERING)
 					.condition(() -> leftHouse() && nextState() == SCATTERING)
 				
