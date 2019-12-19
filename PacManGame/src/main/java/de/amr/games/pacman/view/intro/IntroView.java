212b8a6294608785e8286a17d3667a8fe8627bbc
Armin Reichert <armin.reichert@web.de>
2019-12-19 10:47:30 +0100

    font

--- PacManGame/src/main/java/de/amr/games/pacman/view/intro/IntroView.java
+++ PacManGame/src/main/java/de/amr/games/pacman/view/intro/IntroView.java
@@ -210,8 +210,8 @@ public class IntroView extends StateMachine<IntroViewState, Void> implements Vie
 		case LOADING_MUSIC:
 			loadingAlpha = Math.min(loadingAlpha + 1, 255);
 			pen.color(new Color(255, 255, 255, loadingAlpha));
-			pen.fontSize(16);
-			pen.draw("Loading...", 8, 18);
+			pen.fontSize(20);
+			pen.draw("Loading...", 6, 18);
 			break;
 		case LOGO_SCROLLING_IN:
 			break;
