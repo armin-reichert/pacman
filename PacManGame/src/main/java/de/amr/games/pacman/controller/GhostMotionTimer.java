8f96f0f4a2074978e9c316796bc9553c9f870c0b
Armin Reichert <armin.reichert@web.de>
2019-12-19 16:15:19 +0100

    simplified

--- PacManGame/src/main/java/de/amr/games/pacman/controller/GhostMotionTimer.java
+++ PacManGame/src/main/java/de/amr/games/pacman/controller/GhostMotionTimer.java
@@ -13,78 +13,55 @@ import de.amr.games.pacman.model.Timing;
 import de.amr.statemachine.StateMachine;
 
 /**
- * Controller for the timing of the ghost motion. Ghosts change between chasing
- * and scattering mode during each level in several rounds. The duration of
- * these rounds depends on the level and round. When a ghost becomes frightened,
- * the timer is stopped and the ghost resumes later in that state.
+ * Controller for the timing of the ghost motion. Ghosts change between chasing and scattering mode
+ * during each level in several rounds. The duration of these rounds depends on the level and round.
+ * When a ghost becomes frightened, the timer is stopped and the ghost resumes later in that state.
  * 
  * @author Armin Reichert
  * 
  * @see <a href=
  *      "http://www.gamasutra.com/view/feature/132330/the_pacman_dossier.php?page=3">Gamasutra</a>
  */
-class GhostMotionTimer extends StateMachine<GhostState, Void> {
+public class GhostMotionTimer extends StateMachine<GhostState, Void> {
 
-	static final int[][] SCATTERING_TICKS = {
-			/*@formatter:off*/
-			{ sec(7), sec(7), sec(5), sec(5) }, // Level 1
-			{ sec(7), sec(7), sec(5), 1 },      // Level 2-4
-			{ sec(5), sec(5), sec(5), 1 },      // Level >= 5
-			/*@formatter:on*/
+	/*@formatter:off*/
+	private final int[][] times = {
+		// round 1            round 2            round 3              round 4
+		{  sec(7), sec(20),   sec(7), sec(20),   sec(5), sec(  20),   sec(5), Integer.MAX_VALUE },	// Level 1
+		{  sec(7), sec(20),   sec(7), sec(20),   sec(5), sec(1033),        1, Integer.MAX_VALUE },	// Levels 2-4
+		{  sec(5), sec(20),   sec(5), sec(20),   sec(5), sec(1037),        1, Integer.MAX_VALUE },	// Levels 5+
 	};
+	/*@formatter:on*/
 
-	static final int[][] CHASING_TICKS = {
-			/*@formatter:off*/
-			{ sec(20), sec(20), sec(20),   Integer.MAX_VALUE }, // Level 1
-			{ sec(20), sec(20), sec(1033), Integer.MAX_VALUE }, // Level 2-4
-			{ sec(20), sec(20), sec(1037), Integer.MAX_VALUE }, // Level >= 5
-			/*@formatter:on*/
-	};
-
-	/**
-	 * @param levelNumber current game level number
-	 * @param round       attack round
-	 * @return number of ticks ghost will scatter in this round and level
-	 */
-	static int scatterTicks(int levelNumber, int round) {
-		return SCATTERING_TICKS[(levelNumber == 1) ? 0 : (levelNumber <= 4) ? 1 : 2][Math.min(round, 3)];
+	private int row(int levelNumber) {
+		return levelNumber == 1 ? 0 : levelNumber < 5 ? 1 : 2;
 	}
 
-	/**
-	 * @param levelNumber current game level number
-	 * @param round       attack round
-	 * @return number of ticks ghost will chase in this round and level
-	 */
-	static int chasingTicks(int levelNumber, int round) {
-		return CHASING_TICKS[(levelNumber == 1) ? 0 : (levelNumber <= 4) ? 1 : 2][Math.min(round, 3)];
-	}
-
-	private int round;
+	private int round; // starts with 1
 	private boolean suspended;
 
 	public GhostMotionTimer(PacManGame game) {
 		super(GhostState.class);
-		traceTo(Logger.getLogger("StateMachineLogger"), () -> Timing.FPS);
 		/*@formatter:off*/
 		beginStateMachine()
 			.description("[GhostMotionTimer]")
 			.initialState(SCATTERING)
 		.states()
 			.state(SCATTERING)
-				.timeoutAfter(() -> scatterTicks(game.level.number, round))
+				.timeoutAfter(() -> times[row(game.level.number)][2 * round - 2])
 			.state(CHASING)
-				.timeoutAfter(() -> chasingTicks(game.level.number, round))
-				.onExit(() -> ++round)
+				.timeoutAfter(() -> times[row(game.level.number)][2 * round - 1])
 		.transitions()
 			.when(SCATTERING).then(CHASING).onTimeout()
-			.when(CHASING).then(SCATTERING).onTimeout()
+			.when(CHASING).then(SCATTERING).onTimeout().act(() -> ++round)
 		.endStateMachine();
 		/*@formatter:on*/
+		traceTo(Logger.getLogger("StateMachineLogger"), () -> Timing.FPS);
 	}
 
 	@Override
 	public void init() {
-		round = 0;
+		round = 1;
 		suspended = false;
 		super.init();
 	}
@@ -98,16 +75,16 @@ class GhostMotionTimer extends StateMachine<GhostState, Void> {
 
 	public void suspend() {
 		if (!suspended) {
-			LOGGER.info(() -> String.format("%s: suspended %s, remaining time: %d frames (%.2f seconds)", getDescription(),
-					getState(), state().getTicksRemaining(), state().getTicksRemaining() / 60f));
+			LOGGER.info(() -> String.format("%s: suspended %s, remaining time: %d frames (%.2f seconds)",
+					getDescription(), getState(), state().getTicksRemaining(), state().getTicksRemaining() / 60f));
 			suspended = true;
 		}
 	}
 
 	public void resume() {
 		if (suspended) {
-			LOGGER.info(() -> String.format("%s: resumed %s, remaining time: %d frames (%.2f seconds)", getDescription(),
-					getState(), state().getTicksRemaining(), state().getTicksRemaining() / 60f));
+			LOGGER.info(() -> String.format("%s: resumed %s, remaining time: %d frames (%.2f seconds)",
+					getDescription(), getState(), state().getTicksRemaining(), state().getTicksRemaining() / 60f));
 			suspended = false;
 		}
 	}
