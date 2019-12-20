b401dd027ac551f033ab20ff9e8cc64d99624a4a
Armin Reichert <armin.reichert@web.de>
2019-12-20 17:39:32 +0100

    improved

--- PacManTests/src/main/java/de/amr/games/pacman/test/graph/TakeShortestPathTestApp.java
+++ PacManTests/src/main/java/de/amr/games/pacman/test/graph/TakeShortestPathTestApp.java
@@ -4,7 +4,6 @@ import static de.amr.games.pacman.actor.GhostState.CHASING;
 import static de.amr.games.pacman.actor.GhostState.FRIGHTENED;
 import static de.amr.games.pacman.actor.behavior.Steerings.takingShortestPath;
 
-import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.util.Arrays;
 import java.util.List;
@@ -48,19 +47,18 @@ class TakeShortestPathTestUI extends PlayView implements VisualController {
 
 	final Ghost ghost;
 	final List<Tile> targets;
-	int current;
+	int targetIndex;
 
 	public TakeShortestPathTestUI(PacManGameCast cast) {
 		super(cast);
-		cast.theme().snd_ghost_chase().volume(0);
 		ghost = cast.blinky;
 		targets = Arrays.asList(maze().cornerSE, maze().tileAt(15, 23), maze().tileAt(12, 23), maze().cornerSW,
 				maze().tunnelExitLeft, maze().cornerNW, maze().ghostHouseSeats[0], maze().cornerNE,
 				maze().tunnelExitRight, maze().pacManHome);
-	}
-
-	Tile currentTarget() {
-		return targets.get(current);
+		setShowRoutes(true);
+		setShowStates(true);
+		setShowScores(false);
+		setShowGrid(true);
 	}
 
 	@Override
@@ -68,24 +66,19 @@ class TakeShortestPathTestUI extends PlayView implements VisualController {
 		super.init();
 		game().init();
 		maze().removeFood();
-		current = 0;
-		Steering<Ghost> shortestPath = takingShortestPath(maze(), this::currentTarget);
+		targetIndex = 0;
+		cast.theme().snd_ghost_chase().volume(0);
+		cast().putOnStage(ghost);
+		Steering<Ghost> shortestPath = takingShortestPath(maze(), () -> targets.get(targetIndex));
 		ghost.during(CHASING, shortestPath);
 		ghost.during(FRIGHTENED, shortestPath);
-		cast().putOnStage(ghost);
 		ghost.setState(CHASING);
-		messageColor = Color.YELLOW;
-		messageText = "SPACE toggles ghost state";
-		setShowRoutes(true);
-		setShowStates(true);
-		setShowScores(false);
-		setShowGrid(true);
+		message("SPACE toggles ghost state");
 	}
 
 	private void nextTarget() {
-		current += 1;
-		if (current == targets.size()) {
-			current = 0;
+		if (++targetIndex == targets.size()) {
+			targetIndex = 0;
 			game().enterLevel(game().level.number + 1);
 			maze().removeFood();
 		}
@@ -93,14 +86,14 @@ class TakeShortestPathTestUI extends PlayView implements VisualController {
 
 	@Override
 	public void update() {
+		super.update();
 		if (Keyboard.keyPressedOnce(KeyEvent.VK_SPACE)) {
 			ghost.setState(ghost.getState() == CHASING ? FRIGHTENED : CHASING);
 		}
 		ghost.update();
-		if (ghost.tile().equals(targets.get(current))) {
+		if (ghost.tile().equals(targets.get(targetIndex))) {
 			nextTarget();
 		}
-		super.update();
 	}
 
 	@Override
