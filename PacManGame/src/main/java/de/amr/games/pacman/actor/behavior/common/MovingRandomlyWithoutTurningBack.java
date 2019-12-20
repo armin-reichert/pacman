2cf83cb908c97db1fd744eb85a305198330e70eb
Armin Reichert <armin.reichert@web.de>
2019-12-20 15:33:17 +0100

    fmt

--- PacManGame/src/main/java/de/amr/games/pacman/actor/behavior/common/MovingRandomlyWithoutTurningBack.java
+++ PacManGame/src/main/java/de/amr/games/pacman/actor/behavior/common/MovingRandomlyWithoutTurningBack.java
@@ -1,21 +1,35 @@
 package de.amr.games.pacman.actor.behavior.common;
 
+import static de.amr.datastruct.StreamUtils.permute;
+
 import java.util.Collections;
 
-import de.amr.datastruct.StreamUtils;
 import de.amr.games.pacman.actor.behavior.Steering;
 import de.amr.games.pacman.actor.core.MazeMover;
 import de.amr.games.pacman.model.Direction;
 
+/**
+ * A steering that causes an actor to move randomly through the maze without ever turning back.
+ * 
+ * @author Armin Reichert
+ *
+ * @param <T>
+ *          actor type
+ */
 public class MovingRandomlyWithoutTurningBack<T extends MazeMover> implements Steering<T> {
 
 	@Override
 	public void steer(T actor) {
 		actor.setTargetPath(Collections.emptyList());
 		actor.setTargetTile(null);
-		StreamUtils.permute(Direction.dirs()).filter(dir -> actor.enteredNewTile())
-				.filter(dir -> dir != actor.moveDir().opposite()).filter(actor::canCrossBorderTo).findFirst()
-				.ifPresent(actor::setNextDir);
+		/*@formatter:off*/
+		permute(Direction.dirs())
+			.filter(dir -> actor.enteredNewTile())
+			.filter(dir -> dir != actor.moveDir().opposite())
+			.filter(actor::canCrossBorderTo)
+			.findFirst()
+			.ifPresent(actor::setNextDir);
+		/*@formatter:on*/
 	}
 
 	@Override
