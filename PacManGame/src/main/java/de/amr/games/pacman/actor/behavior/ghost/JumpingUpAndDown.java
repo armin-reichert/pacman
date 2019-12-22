5a1fb5063bbb057819c8de108f4117ff4e5119d1
Armin Reichert <armin.reichert@web.de>
2019-12-22 06:10:13 +0100

    cleanup

--- PacManGame/src/main/java/de/amr/games/pacman/actor/behavior/ghost/JumpingUpAndDown.java
+++ PacManGame/src/main/java/de/amr/games/pacman/actor/behavior/ghost/JumpingUpAndDown.java
@@ -6,29 +6,31 @@ import de.amr.games.pacman.model.Direction;
 import de.amr.games.pacman.model.Maze;
 
 /**
- * Lets a ghost jump up and down.
+ * Lets a ghost jump up and down at its place in the house.
  * 
  * @author Armin Reichert
  */
 public class JumpingUpAndDown implements Steering<Ghost> {
 
-	private final int baseY;
+	private final int placeY;
 
-	public JumpingUpAndDown(Maze maze, int ghostHousePlace) {
-		this.baseY = maze.ghostHouseSeats[ghostHousePlace].y();
+	public JumpingUpAndDown(Maze maze, int place) {
+		placeY = maze.ghostHouseSeats[place].y();
 	}
 
 	@Override
 	public void steer(Ghost ghost) {
-		float dy = ghost.tf.getPosition().y - baseY;
+		float dy = ghost.tf.getPosition().y - placeY;
 		if (dy < -3) {
 			ghost.setNextDir(Direction.DOWN);
 		}
 		else if (dy > 3) {
 			ghost.setNextDir(Direction.UP);
 		}
-		else {
-			ghost.setNextDir(ghost.moveDir());
-		}
+	}
+	
+	@Override
+	public boolean onTrack() {
+		return false;
 	}
 }
