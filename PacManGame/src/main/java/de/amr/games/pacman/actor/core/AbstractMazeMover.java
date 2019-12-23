0ebfd8328eff3fd91de8f534839d10f912ba79be
Armin Reichert <armin.reichert@web.de>
2019-12-23 15:57:15 +0100

    teleporter tracing

--- PacManGame/src/main/java/de/amr/games/pacman/actor/core/AbstractMazeMover.java
+++ PacManGame/src/main/java/de/amr/games/pacman/actor/core/AbstractMazeMover.java
@@ -9,7 +9,9 @@ import java.util.Objects;
 
 import de.amr.easy.game.math.Vector2f;
 import de.amr.games.pacman.model.Direction;
+import de.amr.games.pacman.model.PacManGame;
 import de.amr.games.pacman.model.Tile;
+import de.amr.games.pacman.model.Timing;
 import de.amr.statemachine.core.StateMachine;
 
 /**
@@ -71,6 +73,7 @@ public abstract class AbstractMazeMover extends AbstractMazeResident implements 
 		targetTile = null;
 		targetPath = Collections.emptyList();
 		enteredNewTile = true;
+		teleporting.traceTo(PacManGame.FSM_LOGGER, () -> Timing.FPS);
 		teleporting.init();
 	}
 
