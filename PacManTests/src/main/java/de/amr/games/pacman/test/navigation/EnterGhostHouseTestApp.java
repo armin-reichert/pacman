44f85263544821204bb38b5b7cb9199506420c3b
Armin Reichert <armin.reichert@web.de>
2019-12-28 12:26:47 +0100

    cleanup

--- PacManTests/src/main/java/de/amr/games/pacman/test/navigation/EnterGhostHouseTestApp.java
+++ PacManTests/src/main/java/de/amr/games/pacman/test/navigation/EnterGhostHouseTestApp.java
@@ -11,9 +11,7 @@ import de.amr.easy.game.Application;
 import de.amr.easy.game.input.Keyboard;
 import de.amr.easy.game.view.View;
 import de.amr.easy.game.view.VisualController;
-import de.amr.games.pacman.actor.Ghost;
 import de.amr.games.pacman.actor.PacManGameCast;
-import de.amr.games.pacman.model.Maze;
 import de.amr.games.pacman.model.PacManGame;
 import de.amr.games.pacman.model.Tile;
 import de.amr.games.pacman.theme.ClassicPacManTheme;
@@ -44,13 +42,8 @@ public class EnterGhostHouseTestApp extends Application {
 
 class EnterGhostHouseTestUI extends PlayView implements VisualController {
 
-	final Maze maze;
-	final Ghost inky;
-
 	public EnterGhostHouseTestUI(PacManGameCast cast) {
 		super(cast);
-		maze = cast.game().maze();
-		inky = cast.inky;
 		showRoutes(true);
 		showStates(true);
 		showScores(false);
@@ -58,28 +51,27 @@ class EnterGhostHouseTestUI extends PlayView implements VisualController {
 	}
 
 	@Override
+	public Optional<View> currentView() {
+		return Optional.of(this);
+	}
+
+	@Override
 	public void init() {
 		super.init();
 		game().init();
 		maze().removeFood();
-		cast().setActorOnStage(inky);
+		cast().setActorOnStage(cast().inky);
 		messageColor = Color.YELLOW;
 		messageText = "SPACE = enter / leave house";
 	}
 
 	@Override
 	public void update() {
-		boolean outside = !maze.partOfGhostHouse(inky.tile());
-		if (Keyboard.keyPressedOnce(KeyEvent.VK_SPACE)) {
-			inky.setState(outside ? ENTERING_HOUSE : LEAVING_HOUSE);
-		}
-		inky.update();
 		super.update();
+		boolean outside = !maze().partOfGhostHouse(cast.inky.tile());
+		if (Keyboard.keyPressedOnce(KeyEvent.VK_SPACE)) {
+			cast.inky.setState(outside ? ENTERING_HOUSE : LEAVING_HOUSE);
+		}
+		cast.inky.update();
 	}
-
-	@Override
-	public Optional<View> currentView() {
-		return Optional.of(this);
-	}
-
 }
