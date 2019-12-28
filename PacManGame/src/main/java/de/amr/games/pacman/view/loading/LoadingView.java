1e4c80a849eb3d8c172fcfe0bf7429787bdfa930
Armin Reichert <armin.reichert@web.de>
2019-12-28 16:42:59 +0100

    cosmetics

--- PacManGame/src/main/java/de/amr/games/pacman/view/loading/LoadingView.java
+++ PacManGame/src/main/java/de/amr/games/pacman/view/loading/LoadingView.java
@@ -2,7 +2,14 @@ package de.amr.games.pacman.view.loading;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
+import de.amr.easy.game.ui.sprites.Sprite;
+import de.amr.games.pacman.model.Direction;
+import de.amr.games.pacman.model.Tile;
+import de.amr.games.pacman.theme.GhostColor;
 import de.amr.games.pacman.theme.PacManTheme;
 import de.amr.games.pacman.view.core.AbstractPacManGameView;
 import de.amr.games.pacman.view.core.Pen;
@@ -17,9 +24,21 @@ public class LoadingView extends AbstractPacManGameView {
 	private PacManTheme theme;
 	private int alpha = -1;
 	private int alphaInc;
+	private Sprite pacManRight;
+	private Sprite ghostsLeft[];
+	private int ghostDist[];
+	private Direction direction;
+	private float x, y;
+	private float speed;
 
 	public LoadingView(PacManTheme theme) {
 		this.theme = theme;
+		pacManRight = theme.spr_pacManWalking(Direction.RIGHT.ordinal());
+		ghostsLeft = new Sprite[4];
+		for (GhostColor color : GhostColor.values()) {
+			ghostsLeft[color.ordinal()] = theme.spr_ghostColored(color, Direction.LEFT.ordinal());
+		}
+		ghostDist = new int[4];
 	}
 
 	@Override
@@ -30,6 +49,35 @@ public class LoadingView extends AbstractPacManGameView {
 	@Override
 	public void onThemeChanged(PacManTheme theme) {
 		this.theme = theme;
+	}
+
+	@Override
+	public void init() {
+		super.init();
+		direction = Direction.RIGHT;
+		x = 0;
+		y = 9 * Tile.SIZE;
+		speed = 0.9f;
+	}
+
+	@Override
+	public void update() {
+		super.update();
+		if (x > 40 * Tile.SIZE) {
+			direction = Direction.LEFT;
+			y = 27 * Tile.SIZE;
+			speed = -1.5f;
+			Collections.shuffle(Arrays.asList(ghostsLeft));
+			for (int i = 1; i < 4; ++i) {
+				ghostDist[i] = ghostDist[i - 1] + (5 + new Random().nextInt(5)) * Tile.SIZE;
+			}
+		}
+		else if (x < - 50 * Tile.SIZE) {
+			direction = Direction.RIGHT;
+			y = 9 * Tile.SIZE;
+			speed = 0.9f;
+		}
+		x += speed;
 	}
 
 	@Override
@@ -48,6 +96,14 @@ public class LoadingView extends AbstractPacManGameView {
 			pen.fontSize(16);
 			pen.hcenter("Loading music...", width(), 18);
 			alpha += alphaInc;
+		}
+		if (direction == Direction.RIGHT) {
+			pacManRight.draw(g, x, y);
+		}
+		else {
+			for (int i = 0; i < 4; ++i) {
+				ghostsLeft[i].draw(g, x + ghostDist[i], y);
+			}
 		}
 		fpsView.draw(g);
 	}
