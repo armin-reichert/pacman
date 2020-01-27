b05f9fac8ed59c1e1a636bf6b64e81b2c224dfd1
Armin Reichert <armin.reichert@web.de>
2020-01-27 16:45:18 +0100

    cleanup

--- PacManGame/src/main/java/de/amr/games/pacman/actor/steering/ghost/EnteringGhostHouse.java
+++ PacManGame/src/main/java/de/amr/games/pacman/actor/steering/ghost/EnteringGhostHouse.java
@@ -5,12 +5,14 @@ import static de.amr.games.pacman.actor.steering.ghost.EnteringGhostHouse.Enteri
 import static de.amr.games.pacman.actor.steering.ghost.EnteringGhostHouse.EnteringHouseState.FALLING;
 import static de.amr.games.pacman.actor.steering.ghost.EnteringGhostHouse.EnteringHouseState.MOVING_LEFT;
 import static de.amr.games.pacman.actor.steering.ghost.EnteringGhostHouse.EnteringHouseState.MOVING_RIGHT;
+import static de.amr.games.pacman.model.Direction.DOWN;
+import static de.amr.games.pacman.model.Direction.LEFT;
+import static de.amr.games.pacman.model.Direction.RIGHT;
 
 import de.amr.easy.game.math.Vector2f;
 import de.amr.games.pacman.actor.Ghost;
 import de.amr.games.pacman.actor.steering.Steering;
 import de.amr.games.pacman.actor.steering.ghost.EnteringGhostHouse.EnteringHouseState;
-import de.amr.games.pacman.model.Direction;
 import de.amr.games.pacman.model.Game;
 import de.amr.statemachine.core.StateMachine;
 
@@ -38,21 +40,21 @@ public class EnteringGhostHouse extends StateMachine<EnteringHouseState, Void> i
 					.onEntry(() -> {
 						// target tile is only used for route visualization
 						ghost.setTargetTile(ghost.maze().tileAt(target.roundedX(), target.roundedY()));
-						ghost.setWishDir(Direction.DOWN);
+						ghost.setWishDir(DOWN);
 					})
 					
 			.transitions()
 	
 				.when(AT_DOOR).then(FALLING)
-					.act(() -> ghost.setWishDir(Direction.DOWN))
+					.act(() -> ghost.setWishDir(DOWN))
 	
 				.when(FALLING).then(MOVING_LEFT)
 					.condition(() -> ghost.tf.getY() >= target.y && ghost.tf.getX() > target.x)
-					.act(() -> ghost.setWishDir(Direction.LEFT))
+					.act(() -> ghost.setWishDir(LEFT))
 				
 				.when(FALLING).then(MOVING_RIGHT)
 					.condition(() -> ghost.tf.getY() >= target.y && ghost.tf.getX() < target.x)
-					.act(() -> ghost.setWishDir(Direction.RIGHT))
+					.act(() -> ghost.setWishDir(RIGHT))
 	
 				.when(FALLING).then(AT_PLACE)
 					.condition(() -> ghost.tf.getY() >= target.y && ghost.tf.getX() == target.x)
