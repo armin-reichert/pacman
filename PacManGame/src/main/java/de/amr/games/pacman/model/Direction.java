d6ac5ce1fadf27d5c77c63966de1e0cab9858245
Armin Reichert <armin.reichert@web.de>
2020-05-17 10:55:23 +0200

    cleanup

--- PacManGame/src/main/java/de/amr/games/pacman/model/Direction.java
+++ PacManGame/src/main/java/de/amr/games/pacman/model/Direction.java
@@ -1,46 +1,45 @@
 package de.amr.games.pacman.model;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.stream.Stream;
 
 import de.amr.easy.game.math.Vector2f;
 
 /**
- * Data type for directions in the order UP, RIGHT, DOWN, LEFT.
+ * Directions in the order UP, RIGHT, DOWN, LEFT.
  * 
  * @author Armin Reichert
  */
-public enum Direction implements Iterable<Direction> {
-	UP, RIGHT, DOWN, LEFT;
+public enum Direction {
+	UP(0, -1), RIGHT(1, 0), DOWN(0, 1), LEFT(-1, 0);
 
-	static Vector2f[] VECTOR = { Vector2f.of(0, -1), Vector2f.of(1, 0), Vector2f.of(0, 1), Vector2f.of(-1, 0) };
-	static Direction[] OPPOSITE = { DOWN, LEFT, UP, RIGHT };
-	static Direction[] LEFT_OF = { LEFT, UP, RIGHT, DOWN };
-	static Direction[] RIGHT_OF = { RIGHT, DOWN, LEFT, UP };
-
-	@Override
-	public Iterator<Direction> iterator() {
-		return Arrays.asList(values()).iterator();
-	}
+	static Direction[] OPPOSITE_DIR = { DOWN, LEFT, UP, RIGHT };
+	static Direction[] LEFT_DIR = { LEFT, UP, RIGHT, DOWN };
+	static Direction[] RIGHT_DIR = { RIGHT, DOWN, LEFT, UP };
 
 	public static Stream<Direction> dirs() {
 		return Arrays.stream(values());
 	}
 
+	private final Vector2f vector;
+
+	private Direction(int dx, int dy) {
+		vector = Vector2f.of(dx, dy);
+	}
+
 	public Vector2f vector() {
-		return VECTOR[ordinal()];
+		return vector;
 	}
 
 	public Direction opposite() {
-		return OPPOSITE[ordinal()];
+		return OPPOSITE_DIR[ordinal()];
 	}
 
 	public Direction left() {
-		return LEFT_OF[ordinal()];
+		return LEFT_DIR[ordinal()];
 	}
 
 	public Direction right() {
-		return RIGHT_OF[ordinal()];
+		return RIGHT_DIR[ordinal()];
 	}
 }
