0fe8d58f3f5b9a99163ec476ddaa12af62a54e34
Armin Reichert <armin.reichert@web.de>
2020-05-26 13:34:10 +0200

    make maze view an inner class realized by a state machine

--- PacManGame/src/main/java/de/amr/games/pacman/view/play/SimplePlayView.java
+++ PacManGame/src/main/java/de/amr/games/pacman/view/play/SimplePlayView.java
@@ -1,92 +1,58 @@
 package de.amr.games.pacman.view.play;
 
 import static de.amr.games.pacman.actor.GhostState.DEAD;
+import static de.amr.games.pacman.view.play.SimplePlayView.MazeMode.CROWDED;
+import static de.amr.games.pacman.view.play.SimplePlayView.MazeMode.EMPTY;
+import static de.amr.games.pacman.view.play.SimplePlayView.MazeMode.FLASHING;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
 
+import de.amr.easy.game.Application;
 import de.amr.easy.game.ui.sprites.CyclicAnimation;
 import de.amr.easy.game.ui.sprites.Sprite;
 import de.amr.easy.game.ui.sprites.SpriteAnimation;
 import de.amr.easy.game.view.Pen;
+import de.amr.easy.game.view.View;
 import de.amr.games.pacman.actor.GhostState;
 import de.amr.games.pacman.model.Game;
 import de.amr.games.pacman.model.Symbol;
 import de.amr.games.pacman.model.Tile;
 import de.amr.games.pacman.theme.Theme;
 import de.amr.games.pacman.view.core.PacManGameView;
+import de.amr.statemachine.core.StateMachine;
 
 /**
- * Simple play view providing core functionality.
+ * Simple play view providing the core functionality for playing.
  * 
  * @author Armin Reichert
  */
 public class SimplePlayView extends PacManGameView {
 
-	public enum Mode {
-		EMPTY_MAZE, CROWDED_MAZE, FLASHING_MAZE
+	enum MazeMode {
+		EMPTY, CROWDED, FLASHING
 	}
 
-	protected Mode mode;
-	protected SpriteAnimation energizerBlinking;
-	protected Image imageLife;
-	protected Sprite spriteMazeEmpty;
-	protected Sprite spriteMazeFull;
-	protected Sprite spriteMazeFlashing;
-
 	public boolean showScores = true;
+	private MazeView mazeView;
+	private Image imageLife;
 
 	public SimplePlayView(Game game, Theme theme) {
 		super(game, theme);
-		mode = Mode.CROWDED_MAZE;
-		energizerBlinking = new CyclicAnimation(2);
-		energizerBlinking.setFrameDuration(150);
 		imageLife = theme.spr_pacManWalking(3).frame(1);
-		spriteMazeFull = theme.spr_fullMaze();
-		spriteMazeEmpty = theme.spr_emptyMaze();
-		spriteMazeFlashing = theme.spr_flashingMaze();
-		game.bonus.tf.setPosition(game.maze.bonusTile.centerX(), game.maze.bonusTile.y());
+		mazeView = new MazeView();
 	}
 
 	@Override
 	public void init() {
 		super.init();
-		setEnergizersBlinking(false);
-		showCrowdedMaze();
+		mazeView.init();
 	}
 
 	@Override
 	public void update() {
-		if (mode == Mode.CROWDED_MAZE) {
-			energizerBlinking.update();
-		}
-	}
-
-	public void enableGhostAnimations(boolean enabled) {
-		game.ghostsOnStage().forEach(ghost -> {
-			ghost.sprites.forEach(sprite -> sprite.enableAnimation(enabled));
-		});
-	}
-
-	public float mazeFlashingSeconds() {
-		return game.level.mazeNumFlashes * Theme.MAZE_FLASH_TIME_MILLIS / 1000f;
-	}
-
-	public void showEmptyMaze() {
-		mode = Mode.EMPTY_MAZE;
-	}
-
-	public void showFlashingMaze() {
-		mode = Mode.FLASHING_MAZE;
-	}
-
-	public void showCrowdedMaze() {
-		mode = Mode.CROWDED_MAZE;
-	}
-
-	public void setEnergizersBlinking(boolean blinking) {
-		energizerBlinking.setEnabled(blinking);
+		mazeView.update();
 	}
 
 	@Override
@@ -98,53 +64,36 @@ public class SimplePlayView extends PacManGameView {
 		drawActors(g);
 	}
 
+	public void enableGhostAnimations(boolean enabled) {
+		game.ghostsOnStage().flatMap(ghost -> ghost.sprites.values()).forEach(sprite -> sprite.enableAnimation(enabled));
+	}
+
+	public float mazeFlashingSeconds() {
+		return game.level.mazeNumFlashes * Theme.MAZE_FLASH_TIME_MILLIS / 1000f;
+	}
+
+	public void showEmptyMaze() {
+		mazeView.setState(EMPTY);
+	}
+
+	public void showFlashingMaze() {
+		mazeView.setState(FLASHING);
+	}
+
+	public void showCrowdedMaze() {
+		mazeView.setState(CROWDED);
+	}
+
+	public void setEnergizersBlinking(boolean enabled) {
+		mazeView.energizerBlinking.setEnabled(enabled);
+	}
+
 	protected Color bgColor(Tile tile) {
 		return theme.color_mazeBackground();
 	}
 
 	protected void drawMaze(Graphics2D g) {
-		switch (mode) {
-		case CROWDED_MAZE:
-			drawCrowdedMaze(g);
-			break;
-		case EMPTY_MAZE:
-			drawEmptyMaze(g);
-			break;
-		case FLASHING_MAZE:
-			drawFlashingMaze(g);
-			break;
-		default:
-			break;
-		}
-	}
-
-	protected void drawCrowdedMaze(Graphics2D g) {
-		spriteMazeFull.draw(g, 0, 3 * Tile.SIZE);
-		// hide eaten food
-		game.maze.playingArea().filter(game.maze::isEatenFood).forEach(tile -> {
-			g.setColor(bgColor(tile));
-			g.fillRect(tile.x(), tile.y(), Tile.SIZE, Tile.SIZE);
-		});
-		// hide active energizers when blinking animation is in dark phase
-		if (energizerBlinking.currentFrame() == 1) {
-			game.maze.playingArea().filter(game.maze::isEnergizer).forEach(tile -> {
-				g.setColor(bgColor(tile));
-				g.fillRect(tile.x(), tile.y(), Tile.SIZE, Tile.SIZE);
-			});
-		}
-		// draw door open when any ghost is entering or leaving the house
-		if (game.ghostsOnStage().anyMatch(ghost -> ghost.is(GhostState.ENTERING_HOUSE, GhostState.LEAVING_HOUSE))) {
-			g.setColor(theme.color_mazeBackground());
-			g.fillRect(game.maze.ghostHouseDoorLeft.x(), game.maze.ghostHouseDoorLeft.y(), 2 * Tile.SIZE, Tile.SIZE);
-		}
-	}
-
-	protected void drawEmptyMaze(Graphics2D g) {
-		spriteMazeEmpty.draw(g, 0, 3 * Tile.SIZE);
-	}
-
-	protected void drawFlashingMaze(Graphics2D g) {
-		spriteMazeFlashing.draw(g, 0, 3 * Tile.SIZE);
+		mazeView.draw(g);
 	}
 
 	protected void drawActors(Graphics2D g) {
@@ -219,6 +168,65 @@ public class SimplePlayView extends PacManGameView {
 		for (int i = 0, x = width() - 2 * sz; i < n; ++i, x -= sz) {
 			Symbol symbol = game.levelCounter.get(first + i);
 			g.drawImage(theme.spr_bonusSymbol(symbol).frame(0), x, height() - sz, sz, sz, null);
+		}
+	}
+
+	/**
+	 * Inner class realizing the maze view which can be in any of states EMPTY, CROWDED or FLASHING.
+	 */
+	private class MazeView extends StateMachine<MazeMode, Void> implements View {
+
+		Sprite spriteMazeEmpty, spriteMazeFull, spriteMazeFlashing;
+		SpriteAnimation energizerBlinking;
+
+		public MazeView() {
+			super(MazeMode.class);
+			spriteMazeFull = theme.spr_fullMaze();
+			spriteMazeEmpty = theme.spr_emptyMaze();
+			spriteMazeFlashing = theme.spr_flashingMaze();
+			energizerBlinking = new CyclicAnimation(2);
+			energizerBlinking.setFrameDuration(150);
+			game.bonus.tf.setPosition(game.maze.bonusTile.centerX(), game.maze.bonusTile.y());
+			//@formatter:off
+			beginStateMachine()
+				.description("[Maze View]")
+				.initialState(CROWDED)
+				.states()
+					.state(CROWDED)
+						.onEntry(() -> setEnergizersBlinking(false))
+						.onTick(() -> energizerBlinking.update())
+				.transitions()
+			.endStateMachine();
+			//@formatter:on
+			getTracer().setLogger(Application.LOGGER);
+		}
+
+		@Override
+		public void draw(Graphics2D g) {
+			if (getState() == CROWDED) {
+				spriteMazeFull.draw(g, 0, 3 * Tile.SIZE);
+				// hide eaten food
+				game.maze.playingArea().filter(game.maze::isEatenFood).forEach(tile -> {
+					g.setColor(bgColor(tile));
+					g.fillRect(tile.x(), tile.y(), Tile.SIZE, Tile.SIZE);
+				});
+				// hide active energizers when blinking animation is in dark phase
+				if (energizerBlinking.currentFrame() == 1) {
+					game.maze.playingArea().filter(game.maze::isEnergizer).forEach(tile -> {
+						g.setColor(bgColor(tile));
+						g.fillRect(tile.x(), tile.y(), Tile.SIZE, Tile.SIZE);
+					});
+				}
+				// draw door open when any ghost is entering or leaving the house
+				if (game.ghostsOnStage().anyMatch(ghost -> ghost.is(GhostState.ENTERING_HOUSE, GhostState.LEAVING_HOUSE))) {
+					g.setColor(theme.color_mazeBackground());
+					g.fillRect(game.maze.ghostHouseDoorLeft.x(), game.maze.ghostHouseDoorLeft.y(), 2 * Tile.SIZE, Tile.SIZE);
+				}
+			} else if (getState() == EMPTY) {
+				spriteMazeEmpty.draw(g, 0, 3 * Tile.SIZE);
+			} else if (getState() == FLASHING) {
+				spriteMazeFlashing.draw(g, 0, 3 * Tile.SIZE);
+			}
 		}
 	}
 }
