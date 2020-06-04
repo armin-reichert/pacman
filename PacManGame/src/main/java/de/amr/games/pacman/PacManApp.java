c6000270ff35ca599ab4cbc7dff54a2614df52e0
Armin Reichert <armin.reichert@web.de>
2020-06-04 13:25:24 +0200

    cosmetics

--- PacManGame/src/main/java/de/amr/games/pacman/PacManApp.java
+++ PacManGame/src/main/java/de/amr/games/pacman/PacManApp.java
@@ -29,18 +29,12 @@ public class PacManApp extends Application {
 		@Parameter(names = { "-demoMode" }, description = "Pac-Man moves automatically")
 		public boolean demoMode = false;
 
-		@Parameter(names = {
-				"-ghostsFleeRandomly" }, description = "Frightened ghosts flee randomly (true) or into safe corner (false)", arity = 1)
-		public boolean ghostsFleeRandomly = true;
-
-		@Parameter(names = { "-simpleMode" }, description = "Strips all extra functionality not need for just playing")
-		public boolean simpleMode = false;
-
 		@Parameter(names = { "-ghostsDangerous" }, description = "Ghost collisions are detected", arity = 1)
 		public boolean ghostsDangerous = true;
 
-		@Parameter(names = { "-pathFinder" }, description = "Used path finding algorithm (astar, bfs, bestfs)")
-		public String pathFinder = "astar";
+		@Parameter(names = {
+				"-ghostsFleeRandomly" }, description = "Frightened ghosts flee randomly (true) or into safe corner (false)", arity = 1)
+		public boolean ghostsFleeRandomly = true;
 
 		@Parameter(names = {
 				"-overflowBug" }, description = "Enable overflow bug as in the original Arcade game", arity = 1)
@@ -48,6 +42,12 @@ public class PacManApp extends Application {
 
 		@Parameter(names = { "-pacManImmortable" }, description = "Pac-Man stays alive when killed by ghost")
 		public boolean pacManImmortable = false;
+
+		@Parameter(names = { "-pathFinder" }, description = "Used path finding algorithm (astar, bfs, bestfs)")
+		public String pathFinder = "astar";
+
+		@Parameter(names = { "-simpleMode" }, description = "Strips all extra functionality not need for just playing")
+		public boolean simpleMode = false;
 
 		@Parameter(names = { "-skipIntro" }, description = "Game starts without intro screen")
 		public boolean skipIntro = false;
@@ -75,24 +75,23 @@ public class PacManApp extends Application {
 	@Override
 	protected void printSettings() {
 		super.printSettings();
-		loginfo("\tSimple mode: %s", settings.simpleMode);
-		loginfo("\tDemo mode: %s", settings.demoMode);
-		loginfo("\tGhosts dangerous: %s", settings.ghostsDangerous);
-		loginfo("\tGhosts flee randomly: %s", settings.ghostsFleeRandomly);
-		loginfo("\tPath finder: %s", settings.pathFinder);
-		loginfo("\tOverflow Bug: %s", settings.overflowBug);
-		loginfo("\tPacMan immortable: %s", settings.pacManImmortable);
-		loginfo("\tSkip Intro: %s", settings.skipIntro);
-		loginfo("\tStart level: %d", settings.startLevel);
-		loginfo("User language is %s", texts.getLocale().getDisplayLanguage());
+		printValue("Demo mode", "%s", settings.demoMode);
+		printValue("Ghosts dangerous", "%s", settings.ghostsDangerous);
+		printValue("Ghosts flee randomly", "%s", settings.ghostsFleeRandomly);
+		printValue("Overflow Bug", "%s", settings.overflowBug);
+		printValue("PacMan immortable", "%s", settings.pacManImmortable);
+		printValue("Path finder", "%s", settings.pathFinder);
+		printValue("Simple mode", "%s", settings.simpleMode);
+		printValue("Skip Intro", "%s", settings.skipIntro);
+		printValue("Start level", "%d", settings.startLevel);
+		printValue("User language", "%s", texts.getLocale().getDisplayLanguage());
 	}
 
 	@Override
 	public void init() {
 		PacManStateMachineLogging.setEnabled(false);
 		Theme theme = new ArcadeTheme(); // the only theme yet
-		GameController gameController = settings.simpleMode? new GameController(theme)
-				: new EnhancedGameController(theme);
+		GameController gameController = settings.simpleMode ? new GameController(theme) : new EnhancedGameController(theme);
 		setIcon(theme.spr_ghostFrightened().frame(0));
 		onStateEntry(CLOSED, gameController::saveHiscore);
 		setController(gameController);
