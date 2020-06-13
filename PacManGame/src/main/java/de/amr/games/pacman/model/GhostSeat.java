4d96ec85f2536d42fdc3e927b6e6e1ec51e97a51
Armin Reichert <armin.reichert@web.de>
2020-06-13 09:05:51 +0200

    doc

--- PacManGame/src/main/java/de/amr/games/pacman/model/GhostSeat.java
+++ PacManGame/src/main/java/de/amr/games/pacman/model/GhostSeat.java
@@ -2,15 +2,21 @@ package de.amr.games.pacman.model;
 
 import de.amr.easy.game.math.Vector2f;
 
+/**
+ * A ghost seat has a dedicated exit direction which is the initial move direction of the ghost at
+ * that seat.
+ * 
+ * @author Armin Reichert
+ */
 public class GhostSeat {
 
 	public final Tile tile;
-	public final Direction exitDir;
 	public final Vector2f position;
+	public final Direction exitDir;
 
 	public GhostSeat(int col, int row, Direction dir) {
 		tile = new Tile(col, row);
-		exitDir = dir;
 		position = Vector2f.of(tile.centerX(), tile.y());
+		exitDir = dir;
 	}
 }
