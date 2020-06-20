aa9f6e67471bd6495be43c0a63cbdeeda09d6600
Armin Reichert <armin.reichert@web.de>
2020-06-20 18:28:06 +0200

    cleanup

--- PacManGame/src/main/java/de/amr/games/pacman/view/settings/SpeedCellRenderer.java
+++ PacManGame/src/main/java/de/amr/games/pacman/view/settings/SpeedCellRenderer.java
@@ -1,12 +1,14 @@
 package de.amr.games.pacman.view.settings;
 
+import static de.amr.games.pacman.controller.actor.Ghost.Sanity.CRUISE_ELROY1;
+import static de.amr.games.pacman.controller.actor.Ghost.Sanity.CRUISE_ELROY2;
+
 import java.awt.Color;
 import java.awt.Component;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
-import de.amr.games.pacman.controller.actor.Ghost.Sanity;
 import de.amr.games.pacman.view.settings.GameStateViewModel.Row;
 
 public class SpeedCellRenderer extends DefaultTableCellRenderer {
@@ -18,7 +20,7 @@ public class SpeedCellRenderer extends DefaultTableCellRenderer {
 		GameStateViewModel model = (GameStateViewModel) table.getModel();
 		float speed = (float) value;
 		setText(Formatting.pixelsPerSec(speed));
-		if (row == Row.Blinky.ordinal() && model.game.blinky.sanity.is(Sanity.CRUISE_ELROY1, Sanity.CRUISE_ELROY2)) {
+		if (row == Row.Blinky.ordinal() && model.game.blinky.sanity.is(CRUISE_ELROY1, CRUISE_ELROY2)) {
 			setBackground(new Color(255, 0, 0, 100));
 			setToolTipText(model.game.blinky.sanity.getState().name());
 		} else {
