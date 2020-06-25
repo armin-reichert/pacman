5ce5fabdd2283d46f151a527c43329933eeb94f0
Armin Reichert <armin.reichert@web.de>
2020-06-25 08:53:33 +0200

    cosmetics

--- PacManGame/src/main/java/de/amr/games/pacman/view/dashboard/level/GameLevelView.java
+++ PacManGame/src/main/java/de/amr/games/pacman/view/dashboard/level/GameLevelView.java
@@ -1,16 +1,33 @@
 package de.amr.games.pacman.view.dashboard.level;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 
 import de.amr.easy.game.controller.Lifecycle;
 import de.amr.games.pacman.controller.GameController;
 import net.miginfocom.swing.MigLayout;
 
 public class GameLevelView extends JPanel implements Lifecycle {
+
+	static class VariableFieldRenderer extends DefaultTableCellRenderer {
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+				int row, int column) {
+			JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+			if (row < 4) {
+				Font bold = new Font(label.getFont().getFamily(), Font.BOLD, label.getFont().getSize());
+				label.setFont(bold);
+			}
+			return this;
+		}
+	}
 
 	private GameController controller;
 	private JTable table;
@@ -27,6 +44,7 @@ public class GameLevelView extends JPanel implements Lifecycle {
 		content.add(scrollPane, "cell 0 0,grow");
 
 		table = new JTable();
+		table.setRowHeight(17);
 		table.setRowSelectionAllowed(false);
 		scrollPane.setViewportView(table);
 	}
@@ -44,6 +62,8 @@ public class GameLevelView extends JPanel implements Lifecycle {
 			tableModel = new GameLevelTableModel();
 		}
 		table.setModel(tableModel);
+		table.getColumnModel().getColumn(0).setCellRenderer(new VariableFieldRenderer());
+		table.getColumnModel().getColumn(1).setCellRenderer(new VariableFieldRenderer());
 	}
 
 	@Override
