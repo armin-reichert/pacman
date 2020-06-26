79a97d979386fd0d851be7a7f96b91229ac1330f
Armin Reichert <armin.reichert@web.de>
2020-06-26 08:06:25 +0200

    cleanup

--- PacManGame/src/main/java/de/amr/games/pacman/view/dashboard/states/GameStateTable.java
+++ PacManGame/src/main/java/de/amr/games/pacman/view/dashboard/states/GameStateTable.java
@@ -7,16 +7,17 @@ import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableModel;
 
+import de.amr.easy.game.controller.Lifecycle;
 import de.amr.games.pacman.view.dashboard.states.GameStateTableModel.ColumnInfo;
-import de.amr.games.pacman.view.dashboard.util.UniversalFormatter;
 import de.amr.games.pacman.view.dashboard.util.Formatting;
+import de.amr.games.pacman.view.dashboard.util.UniversalFormatter;
 
 /**
  * Displays detailed information about the actors in the game that is updated at every tick.
  * 
  * @author Armin Reichert
  */
-public class GameStateTable extends JTable {
+public class GameStateTable extends JTable implements Lifecycle {
 
 	public GameStateTable() {
 		super(new GameStateTableModel());
@@ -26,29 +27,31 @@ public class GameStateTable extends JTable {
 	@Override
 	public void setModel(TableModel model) {
 		super.setModel(model);
-		configureRenderers();
+		init();
 	}
 
+	@Override
+	public void init() {
+		UniversalFormatter tileFmt = new UniversalFormatter();
+		tileFmt.fnHilightCondition = c -> record(c.row) != null && record(c.row).pacManCollision;
+		format(ColumnInfo.Tile, tileFmt);
+
+		UniversalFormatter speedFmt = new UniversalFormatter();
+		speedFmt.fnHilightCondition = c -> c.row == ROW_BLINKY && record(ROW_PACMAN).speed <= record(ROW_BLINKY).speed;
+		format(ColumnInfo.Speed, speedFmt);
+
+		UniversalFormatter ticksFmt = new UniversalFormatter();
+		ticksFmt.fnTextFormat = c -> Formatting.ticksAndSeconds((int) c.value);
+		format(ColumnInfo.Remaining, ticksFmt);
+		format(ColumnInfo.Duration, ticksFmt);
+	}
+
+	@Override
 	public void update() {
 		getGameStateTableModel().update();
 	}
 
-	private void configureRenderers() {
-		UniversalFormatter tileFormatter = new UniversalFormatter();
-		tileFormatter.fnHilightCondition = c -> record(c.row) != null && record(c.row).pacManCollision;
-		renderer(ColumnInfo.Tile, tileFormatter);
-
-		UniversalFormatter speedFormatter = new UniversalFormatter();
-		speedFormatter.fnHilightCondition = c -> c.row == ROW_BLINKY && record(ROW_PACMAN).speed <= record(ROW_BLINKY).speed;
-		renderer(ColumnInfo.Speed, speedFormatter);
-
-		UniversalFormatter ticksFormatter = new UniversalFormatter();
-		ticksFormatter.fnTextFormat = c -> Formatting.ticksAndSeconds((int) c.value);
-		renderer(ColumnInfo.Remaining, ticksFormatter);
-		renderer(ColumnInfo.Duration, ticksFormatter);
-	}
-
-	private void renderer(ColumnInfo columnInfo, TableCellRenderer renderer) {
+	private void format(ColumnInfo columnInfo, TableCellRenderer renderer) {
 		getColumnModel().getColumn(columnInfo.ordinal()).setCellRenderer(renderer);
 	}
 
