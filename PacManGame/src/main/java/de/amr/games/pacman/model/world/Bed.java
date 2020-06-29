4813b1c790e1a6e7716752ef87502a8fe4371f59
Armin Reichert <armin.reichert@web.de>
2020-06-29 16:20:46 +0200

    cleanup

--- PacManGame/src/main/java/de/amr/games/pacman/model/world/Bed.java
+++ PacManGame/src/main/java/de/amr/games/pacman/model/world/Bed.java
@@ -4,14 +4,14 @@ import de.amr.easy.game.math.Vector2f;
 import de.amr.games.pacman.model.Direction;
 
 /**
- * A seat is a maze position with a start direction which is the initial move direction of the
- * creature residing at that seat.
+ * A bed is a maze position defining an initial move direction for the creature residing at that
+ * position.
  * 
  * @author Armin Reichert
  */
 public class Bed {
 
-	public int number;
+	public final int number;
 	public final Tile tile;
 	public final Vector2f position;
 	public final Direction startDir;
