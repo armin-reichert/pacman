f61d08ef963bd562c7791c62346f092db55e198c
Armin Reichert <armin.reichert@web.de>
2020-07-01 07:21:42 +0200

    improved

--- PacManGame/src/main/java/de/amr/games/pacman/model/world/ArcadeWorld.java
+++ PacManGame/src/main/java/de/amr/games/pacman/model/world/ArcadeWorld.java
@@ -2,7 +2,6 @@ package de.amr.games.pacman.model.world;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -59,48 +58,36 @@ class ArcadeWorld extends AbstractWorld {
 
 	public ArcadeWorld() {
 		worldMap = new WorldMap(DATA);
+		pacManBed = new Bed(4, 13, 26, Direction.RIGHT);
+		houses.add(ghostHouse(16, 11, 6, 4));
+		bonusTile = Tile.at(13, 20);
+		portals.add(new Portal(Tile.at(-1, 17), Tile.at(28, 17)));
+		oneWayTiles.addAll(Arrays.asList(
+		//@formatter:off
+			new OneWayTile(12, 13, Direction.DOWN), 
+			new OneWayTile(15, 13, Direction.DOWN),
+			new OneWayTile(12, 25, Direction.DOWN), 
+			new OneWayTile(15, 25, Direction.DOWN)
+		//@formatter:on
+		));
+	}
 
-		// ghost house
-
-		int top = 16, left = 11, w = 6, h = 4;
+	private House ghostHouse(int top, int left, int w, int h) {
 		Set<Tile> room = new HashSet<>();
 		for (int row = top; row < top + h; ++row) {
 			for (int col = left; col < left + w; ++col) {
 				room.add(Tile.at(col, row));
 			}
 		}
-
 		Door door = new Door(Direction.DOWN, Tile.at(left + 2, top - 1), Tile.at(left + 3, top - 1));
-
-		pacManBed = new Bed(4, 13, 26, Direction.RIGHT);
+		return new House(room, Arrays.asList(door), Arrays.asList(
 		//@formatter:off
-		List<Bed> beds = Arrays.asList(
-				new Bed(0, left + 2, top - 2, Direction.LEFT),
-				new Bed(1, left,     top + 1, Direction.UP), 
-				new Bed(2, left + 2, top + 1, Direction.DOWN),
-				new Bed(3, left + 4, top + 1, Direction.UP));
-		//@formatter:on
-
-		houses.add(new House(room, Arrays.asList(door), beds));
-
-		// bonus location
-
-		bonusTile = Tile.at(13, 20);
-
-		// portals
-
-		portals.add(new Portal(Tile.at(-1, 17), Tile.at(28, 17)));
-
-		// one-way tiles
-
-		//@formatter:off
-		oneWayTiles.addAll(Arrays.asList(
-				new OneWayTile(12, 13, Direction.DOWN), 
-				new OneWayTile(15, 13, Direction.DOWN),
-				new OneWayTile(12, 25, Direction.DOWN), 
-				new OneWayTile(15, 25, Direction.DOWN)
-		));
-		//@formatter:on
+			new Bed(0, left + 2, top - 2, Direction.LEFT),
+			new Bed(1, left,     top + 1, Direction.UP), 
+			new Bed(2, left + 2, top + 1, Direction.DOWN),
+			new Bed(3, left + 4, top + 1, Direction.UP))
+			//@formatter:on
+		);
 	}
 
 	@Override
