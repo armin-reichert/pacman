594de1ed96658fdedf789e598e6cd96229c91460
Armin Reichert <armin.reichert@web.de>
2020-07-09 09:38:05 +0200

    doc

--- PacManGame/src/main/java/de/amr/games/pacman/controller/actor/GhostSanityControl.java
+++ PacManGame/src/main/java/de/amr/games/pacman/controller/actor/GhostSanityControl.java
@@ -7,6 +7,13 @@ import static de.amr.games.pacman.controller.actor.GhostSanity.INFECTABLE;
 import de.amr.games.pacman.model.game.Game;
 import de.amr.statemachine.core.StateMachine;
 
+/**
+ * When Blinky takes part in a game, he becomes mentally ill (transforms into a "Cruise Elroy")
+ * whenever the number of remaining food reaches certain numbers which depend on the game level.
+ * This state machine controls the sanity state of Blinky.
+ * 
+ * @author Armin Reichert
+ */
 public class GhostSanityControl extends StateMachine<GhostSanity, Void> {
 
 	public GhostSanityControl(Game game, String ghostName, GhostSanity initialSanity) {
