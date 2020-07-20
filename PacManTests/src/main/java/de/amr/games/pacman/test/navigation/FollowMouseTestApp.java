e595c810e68147b7d10bd8d61705ec87f6561cf8
Armin Reichert <armin.reichert@web.de>
2020-07-20 08:37:47 +0200

    initialize mouse position to center of window

--- PacManTests/src/main/java/de/amr/games/pacman/test/navigation/FollowMouseTestApp.java
+++ PacManTests/src/main/java/de/amr/games/pacman/test/navigation/FollowMouseTestApp.java
@@ -3,9 +3,14 @@ package de.amr.games.pacman.test.navigation;
 import static de.amr.games.pacman.controller.creatures.ghost.GhostState.CHASING;
 import static de.amr.games.pacman.controller.steering.api.AnimalMaster.you;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+
 import de.amr.easy.game.Application;
 import de.amr.easy.game.config.AppSettings;
 import de.amr.easy.game.input.Mouse;
+import de.amr.easy.game.ui.AppShell;
+import de.amr.games.pacman.model.world.api.Bed;
 import de.amr.games.pacman.model.world.api.Tile;
 import de.amr.games.pacman.test.TestUI;
 
@@ -31,7 +36,7 @@ public class FollowMouseTestApp extends Application {
 
 class FollowMouseTestUI extends TestUI {
 
-	private Tile mousePosition = Tile.at(0, 0);
+	private Tile mousePosition = null;
 
 	@Override
 	public void init() {
@@ -46,6 +51,20 @@ class FollowMouseTestUI extends TestUI {
 
 	@Override
 	public void update() {
+		if (mousePosition == null) {
+			try {
+				AppShell shell = Application.app().shell().get();
+				int x = shell.getX() + shell.getWidth() / 2;
+				int y = shell.getY() + shell.getHeight() / 2;
+				mousePosition = Tile.at(x / Tile.SIZE, y / Tile.SIZE);
+				Robot robot = new Robot();
+				robot.mouseMove(x, y);
+			} catch (AWTException e) {
+				Bed bed = world.house(0).bed(0);
+				mousePosition = Tile.at(bed.col(), bed.row());
+				e.printStackTrace();
+			}
+		}
 		if (Mouse.moved()) {
 			mousePosition = Tile.at(Mouse.getX() / Tile.SIZE, Mouse.getY() / Tile.SIZE);
 		}
