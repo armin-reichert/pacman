d6d6d3f7f3b164c504edc0ae88ff956b4653541a
Armin Reichert <armin.reichert@web.de>
2020-07-25 08:27:35 +0200

    improved

--- PacManGame/src/main/java/de/amr/games/pacman/controller/steering/api/AnimalMaster.java
+++ PacManGame/src/main/java/de/amr/games/pacman/controller/steering/api/AnimalMaster.java
@@ -1,8 +1,8 @@
 package de.amr.games.pacman.controller.steering.api;
 
+import java.util.Objects;
 import java.util.function.Supplier;
 
-import de.amr.games.pacman.controller.creatures.api.Creature;
 import de.amr.games.pacman.controller.creatures.ghost.Ghost;
 import de.amr.games.pacman.controller.creatures.ghost.GhostState;
 import de.amr.games.pacman.controller.creatures.pacman.PacMan;
@@ -21,51 +21,78 @@ import de.amr.games.pacman.model.world.components.House;
 
 public class AnimalMaster {
 
-	private Creature<?, ?> animal;
+	private Ghost ghost;
+	private PacMan pacMan;
 	private GhostState ghostState;
 
+	private void ensureGhost() {
+		if (ghost == null) {
+			throw new IllegalStateException("Who do you mean?");
+		}
+	}
+
+	private void ensureGhostState() {
+		if (ghostState == null) {
+			throw new IllegalStateException("When should I do that?");
+		}
+	}
+
+	private void ensureAny() {
+		if (pacMan == null && ghost == null) {
+			throw new IllegalStateException("Who do you mean?");
+		}
+	}
+
 	public static AnimalMaster you(Ghost ghost) {
-		AnimalMaster builder = new AnimalMaster();
-		builder.animal = ghost;
-		return builder;
+		AnimalMaster master = new AnimalMaster();
+		master.ghost = Objects.requireNonNull(ghost);
+		return master;
 	}
 
 	public static AnimalMaster you(PacMan pacMan) {
-		AnimalMaster builder = new AnimalMaster();
-		builder.animal = pacMan;
-		return builder;
+		AnimalMaster master = new AnimalMaster();
+		master.pacMan = Objects.requireNonNull(pacMan);
+		return master;
 	}
 
 	public AnimalMaster when(GhostState state) {
-		this.ghostState = state;
+		ensureGhost();
+		ghostState = Objects.requireNonNull(state);
 		return this;
 	}
 
 	public HeadsForTargetTileBuilder headFor() {
+		ensureAny();
 		return new HeadsForTargetTileBuilder();
 	}
 
 	public BouncesOnBedBuilder bounceOnBed() {
+		ensureGhost();
 		return new BouncesOnBedBuilder();
 	}
 
 	public EntersHouseAndGoesToBedBuilder enterHouseAndGoToBed() {
+		ensureGhost();
 		return new EntersHouseAndGoesToBedBuilder();
 	}
 
 	public LeavesHouseBuilder leaveHouse() {
+		ensureGhost();
 		return new LeavesHouseBuilder();
 	}
 
 	public MovesRandomlyBuilder moveRandomly() {
+		ensureAny();
 		return new MovesRandomlyBuilder();
 	}
 
 	public FleeToSafeTileBuilder fleeToSafeTile() {
+		ensureGhost();
 		return new FleeToSafeTileBuilder();
 	}
 
 	public FollowsKeysBuilder followTheKeys() {
+		ensureAny();
 		return new FollowsKeysBuilder();
 	}
 
@@ -74,12 +101,13 @@ public class AnimalMaster {
 		private Bed bed;
 
 		public BouncesOnBedBuilder bed(Bed bed) {
-			this.bed = bed;
+			this.bed = Objects.requireNonNull(bed);
 			return this;
 		}
 
 		public Steering<Ghost> ok() {
-			Ghost ghost = (Ghost) animal;
+			ensureGhost();
+			ensureGhostState();
 			Steering<Ghost> steering = new BouncingOnBed(bed != null ? bed : ghost.bed());
 			ghost.behavior(ghostState, steering);
 			return steering;
@@ -91,12 +119,13 @@ public class AnimalMaster {
 		private Bed bed;
 
 		public EntersHouseAndGoesToBedBuilder bed(Bed bed) {
-			this.bed = bed;
+			this.bed = Objects.requireNonNull(bed);
 			return this;
 		}
 
 		public Steering<Ghost> ok() {
-			Ghost ghost = (Ghost) animal;
+			ensureGhost();
+			ensureGhostState();
 			Steering<Ghost> steering = new EnteringHouseAndGoingToBed(ghost, bed != null ? bed : ghost.bed());
 			ghost.behavior(ghostState, steering);
 			return steering;
@@ -108,7 +137,7 @@ public class AnimalMaster {
 		private Supplier<Tile> fnTargetTile;
 
 		public HeadsForTargetTileBuilder tile(Supplier<Tile> fnTargetTile) {
-			this.fnTargetTile = fnTargetTile;
+			this.fnTargetTile = Objects.requireNonNull(fnTargetTile);
 			return this;
 		}
 
@@ -121,16 +150,13 @@ public class AnimalMaster {
 		}
 
 		public Steering<?> ok() {
-			if (animal instanceof Ghost) {
-				Ghost ghost = (Ghost) animal;
-				Steering<Ghost> steering = new HeadingForTargetTile<>(ghost, fnTargetTile);
-				ghost.behavior(ghostState, steering);
-				return steering;
-			} else if (animal instanceof PacMan) {
-				PacMan pacMan = (PacMan) animal;
-				Steering<PacMan> steering = new HeadingForTargetTile<>(pacMan, fnTargetTile);
-				pacMan.behavior(PacManState.RUNNING, steering);
-				return steering;
+			if (ghost != null) {
+				ensureGhostState();
+				ghost.behavior(ghostState, new HeadingForTargetTile<>(ghost, fnTargetTile));
+				return ghost.steering();
+			} else if (pacMan != null) {
+				pacMan.behavior(PacManState.RUNNING, new HeadingForTargetTile<>(pacMan, fnTargetTile));
+				return pacMan.steering();
 			}
 			throw new IllegalStateException();
 		}
@@ -141,32 +167,30 @@ public class AnimalMaster {
 		private House house;
 
 		public LeavesHouseBuilder house(House house) {
-			this.house = house;
+			this.house = Objects.requireNonNull(house);
 			return this;
 		}
 
 		public Steering<Ghost> ok() {
-			Ghost ghost = (Ghost) animal;
-			Steering<Ghost> steering = new LeavingHouse(house);
-			ghost.behavior(ghostState, steering);
-			return steering;
+			ensureGhost();
+			ensureGhostState();
+			ghost.behavior(ghostState, new LeavingHouse(house));
+			return ghost.steering();
 		}
 	}
 
 	public class MovesRandomlyBuilder {
 
 		public Steering<?> ok() {
-			if (animal instanceof Ghost) {
-				Ghost ghost = (Ghost) animal;
+			if (ghost != null) {
+				ensureGhostState();
 				ghost.behavior(ghostState, new RandomMovement<>());
 				return ghost.steering();
-			} else if (animal instanceof PacMan) {
-				PacMan pacMan = (PacMan) animal;
+			} else if (pacMan != null) {
 				pacMan.behavior(PacManState.RUNNING, new RandomMovement<>());
 				return pacMan.steering();
-			} else {
-				throw new IllegalStateException();
 			}
+			throw new IllegalStateException();
 		}
 	}
 
@@ -175,17 +199,16 @@ public class AnimalMaster {
 		private MobileLifeform attacker;
 
 		public FleeToSafeTileBuilder from(MobileLifeform attacker) {
-			this.attacker = attacker;
+			this.attacker = Objects.requireNonNull(attacker);
 			return this;
 		}
 
 		public Steering<Ghost> ok() {
-			Ghost ghost = (Ghost) animal;
-			Steering<Ghost> steering = new FleeingToSafeTile(ghost, attacker);
-			ghost.behavior(ghostState, steering);
-			return steering;
+			ensureGhost();
+			ensureGhostState();
+			ghost.behavior(ghostState, new FleeingToSafeTile(ghost, attacker));
+			return ghost.steering();
 		}
-
 	}
 
 	public class FollowsKeysBuilder {
@@ -201,17 +224,15 @@ public class AnimalMaster {
 		}
 
 		public Steering<?> ok() {
-			if (animal instanceof Ghost) {
-				Ghost ghost = (Ghost) animal;
+			if (ghost != null) {
+				ensureGhostState();
 				ghost.behavior(ghostState, new FollowingKeys<>(up, right, down, left));
 				return ghost.steering();
-			} else if (animal instanceof PacMan) {
-				PacMan pacMan = (PacMan) animal;
+			} else if (pacMan != null) {
 				pacMan.behavior(PacManState.RUNNING, new FollowingKeys<>(up, right, down, left));
 				return pacMan.steering();
-			} else {
-				throw new IllegalStateException();
 			}
+			throw new IllegalStateException();
 		}
 	}
 }
