3c69224c669d38164818a7ea527404a2b3d40292
Armin Reichert <armin.reichert@web.de>
2020-07-26 07:44:44 +0200

    doc

--- PacManGame/src/main/java/de/amr/games/pacman/controller/steering/api/Steering.java
+++ PacManGame/src/main/java/de/amr/games/pacman/controller/steering/api/Steering.java
@@ -3,7 +3,7 @@ package de.amr.games.pacman.controller.steering.api;
 import de.amr.games.pacman.model.world.api.MobileLifeform;
 
 /**
- * Interface for steering of actors.
+ * Interface for steering of lifeforms through their world.
  * 
  * @author Armin Reichert
  */
@@ -11,36 +11,36 @@ import de.amr.games.pacman.model.world.api.MobileLifeform;
 public interface Steering<M extends MobileLifeform> {
 
 	/**
-	 * Steers the actor towards its target tile or wherever it should move in its current state.
+	 * Steers the lifeform in its current state.
 	 * 
 	 * @param mover the steered lifeform
 	 */
 	void steer(M mover);
 
 	/**
-	 * Some steerings needs to be initialized.
+	 * Some steerings needs an initial step.
 	 */
 	default void init() {
 	}
 
 	/**
-	 * Triggers the steering once, even if its normal preconditions are not fulfilled.
+	 * Triggers the steering once, even if its preconditions are not fulfilled.
 	 */
 	default void force() {
 	}
 
 	/**
-	 * Tells if the steering has competed. The default is {@code false}.
+	 * Some steerings have a defined end state.
 	 * 
-	 * @return if steering has completed.
+	 * @return tells if the steering is complete (default: {@code false}).
 	 */
 	default boolean isComplete() {
 		return false;
 	}
 
 	/**
-	 * @return tells if the steering requires that moving always keeps the actor aligned with the grid.
-	 *         The default is {@code false}.
+	 * @return tells if the steering requires that the lifeform stays aligned with the grid (default is
+	 *         {@code false}).
 	 */
 	default boolean requiresGridAlignment() {
 		return false;
