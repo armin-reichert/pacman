6cae1611fb1f5f3b5aadfa87818b99b3ad5bdb5c
Armin Reichert <armin.reichert@web.de>
2020-07-26 19:00:35 +0200

    refactoring

--- PacManGame/src/main/java/de/amr/games/pacman/view/api/PacManGameView.java
+++ PacManGame/src/main/java/de/amr/games/pacman/view/api/PacManGameView.java
@@ -2,6 +2,7 @@ package de.amr.games.pacman.view.api;
 
 import de.amr.easy.game.controller.Lifecycle;
 import de.amr.easy.game.view.View;
+import de.amr.games.pacman.view.theme.api.Themeable;
 
 /**
  * All views of the Pac-Man game are themeable and have a lifecycle.
---
+++ PacManGame/src/main/java/de/amr/games/pacman/view/theme/api/Themeable.java
@@ -0,0 +1,8 @@
+package de.amr.games.pacman.view.theme.api;
+
+public interface Themeable {
+
+	Theme getTheme();
+
+	void setTheme(Theme theme);
+}
