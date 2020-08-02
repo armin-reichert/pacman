0547812e74ae42a2c641c03684f97577e5c7b4a3
Armin Reichert <armin.reichert@web.de>
2020-08-02 12:20:56 +0200

    cleanup

--- PacManGame/src/main/java/de/amr/games/pacman/model/world/core/ByteMap.java
+++ PacManGame/src/main/java/de/amr/games/pacman/model/world/core/ByteMap.java
@@ -11,13 +11,20 @@ public class ByteMap {
 
 	private final byte[][] data;
 
+	private static void rangeCheck(int index) {
+		if (index < 0 || index > 7) {
+			throw new IllegalArgumentException("Bit index out of range [0..7]: " + index);
+		}
+	}
+
 	/**
 	 * @param row      a row
 	 * @param col      a column
 	 * @param bitIndex a bit index (0..7)
 	 * @return the boolean value of the bit at the given position and index
 	 */
-	public boolean is(int row, int col, short bitIndex) {
+	public boolean is(int row, int col, int bitIndex) {
+		rangeCheck(bitIndex);
 		return (data[row][col] & (1 << bitIndex)) != 0;
 	}
 
@@ -28,7 +35,8 @@ public class ByteMap {
 	 * @param col      a column
 	 * @param bitIndex a bit index (0..7)
 	 */
-	public void set0(int row, int col, short bitIndex) {
+	public void set0(int row, int col, int bitIndex) {
+		rangeCheck(bitIndex);
 		data[row][col] &= ~(1 << bitIndex);
 	}
 
@@ -39,7 +47,8 @@ public class ByteMap {
 	 * @param col      a column
 	 * @param bitIndex a bit index (0..7)
 	 */
-	public void set1(int row, int col, short bitIndex) {
+	public void set1(int row, int col, int bitIndex) {
+		rangeCheck(bitIndex);
 		data[row][col] |= (1 << bitIndex);
 	}
 
@@ -47,12 +56,17 @@ public class ByteMap {
 	 * Creates a new map using a copy of the given byte array. The rows of the byte array must all have
 	 * the same length.
 	 * 
-	 * @param byteArray byte array
+	 * @param array2D two-dimensional byte array
 	 */
-	public ByteMap(byte[][] byteArray) {
-		data = new byte[byteArray.length][];
-		for (int i = 0; i < byteArray.length; ++i) {
-			data[i] = Arrays.copyOf(byteArray[i], byteArray[0].length);
+	public ByteMap(byte[][] array2D) {
+		data = new byte[array2D.length][];
+		int width = array2D[0].length;
+		for (int i = 0; i < array2D.length; ++i) {
+			byte[] row = array2D[i];
+			if (row.length != width) {
+				throw new IllegalStateException("Rows must all have the same length");
+			}
+			data[i] = Arrays.copyOf(row, width);
 		}
 	}
 
