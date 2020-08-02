e288be84ce80467cd7ce945da808c165ee1733f9
Armin Reichert <armin.reichert@web.de>
2020-08-02 12:09:16 +0200

    doc

--- PacManGame/src/main/java/de/amr/games/pacman/model/world/core/ByteMap.java
+++ PacManGame/src/main/java/de/amr/games/pacman/model/world/core/ByteMap.java
@@ -9,20 +9,46 @@ import java.util.Arrays;
  */
 public class ByteMap {
 
-	public final byte[][] data;
+	private final byte[][] data;
 
-	public boolean is(int row, int col, byte bit) {
-		return (data[row][col] & (1 << bit)) != 0;
+	/**
+	 * @param row      a row
+	 * @param col      a column
+	 * @param bitIndex a bit index (0..7)
+	 * @return the boolean value of the bit at the given position and index
+	 */
+	public boolean is(int row, int col, short bitIndex) {
+		return (data[row][col] & (1 << bitIndex)) != 0;
 	}
 
-	public void set0(int row, int col, byte bit) {
-		data[row][col] &= ~(1 << bit);
+	/**
+	 * Clears the bit at the given map position and index
+	 * 
+	 * @param row      a row
+	 * @param col      a column
+	 * @param bitIndex a bit index (0..7)
+	 */
+	public void set0(int row, int col, short bitIndex) {
+		data[row][col] &= ~(1 << bitIndex);
 	}
 
-	public void set1(int row, int col, byte bit) {
-		data[row][col] |= (1 << bit);
+	/**
+	 * Sets the bit at the given map position and index
+	 * 
+	 * @param row      a row
+	 * @param col      a column
+	 * @param bitIndex a bit index (0..7)
+	 */
+	public void set1(int row, int col, short bitIndex) {
+		data[row][col] |= (1 << bitIndex);
 	}
 
+	/**
+	 * Creates a new map using a copy of the given byte array. The rows of the byte array must all have
+	 * the same length.
+	 * 
+	 * @param byteArray byte array
+	 */
 	public ByteMap(byte[][] byteArray) {
 		data = new byte[byteArray.length][];
 		for (int i = 0; i < byteArray.length; ++i) {
@@ -30,10 +56,16 @@ public class ByteMap {
 		}
 	}
 
+	/**
+	 * @return the number of columns of the map
+	 */
 	public int getWidth() {
 		return data[0].length;
 	}
 
+	/**
+	 * @return the number of rows of the map
+	 */
 	public int getHeight() {
 		return data.length;
 	}
