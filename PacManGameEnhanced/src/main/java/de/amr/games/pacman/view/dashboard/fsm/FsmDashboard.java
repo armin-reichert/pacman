ee6b9d092a16887c231c4bdd78eed3653f2323cc
Armin Reichert <armin.reichert@web.de>
2020-08-14 10:05:57 +0200

    zoom all

--- PacManGameEnhanced/src/main/java/de/amr/games/pacman/view/dashboard/fsm/FsmDashboard.java
+++ PacManGameEnhanced/src/main/java/de/amr/games/pacman/view/dashboard/fsm/FsmDashboard.java
@@ -2,13 +2,18 @@ package de.amr.games.pacman.view.dashboard.fsm;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.KeyStroke;
 
 import de.amr.easy.game.ui.widgets.MultiPanel;
 
@@ -28,9 +33,29 @@ public class FsmDashboard extends JFrame {
 		}
 	}
 
+	private Action actionZoomAllIn = new AbstractAction() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			for (FsmGraphView view : views) {
+				view.actionZoomIn.actionPerformed(e);
+			}
+		}
+	};
+
+	private Action actionZoomAllOut = new AbstractAction() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			for (FsmGraphView view : views) {
+				view.actionZoomOut.actionPerformed(e);
+			}
+		}
+	};
+
 	private final FsmModel model;
 	private MultiPanel multiPanel;
-	private FsmGraphView[] view = new FsmGraphView[4];
+	private FsmGraphView[] views = new FsmGraphView[4];
 
 	public FsmDashboard(FsmModel model) {
 		this.model = model;
@@ -41,11 +66,15 @@ public class FsmDashboard extends JFrame {
 		for (int i = 0; i < 4; ++i) {
 			multiPanel.getComboBox(i).setModel(new FsmSelectionModel(model));
 			multiPanel.getComboBox(i).addItemListener(this::onFsmSelection);
-			view[i] = new FsmGraphView();
-			multiPanel.getPanel(i).add(view[i], BorderLayout.CENTER);
-			multiPanel.getToolBar(i).add(view[i].actionZoomIn);
-			multiPanel.getToolBar(i).add(view[i].actionZoomOut);
+			views[i] = new FsmGraphView();
+			multiPanel.getPanel(i).add(views[i], BorderLayout.CENTER);
+			multiPanel.getToolBar(i).add(views[i].actionZoomIn);
+			multiPanel.getToolBar(i).add(views[i].actionZoomOut);
 		}
+		multiPanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke('+'), actionZoomAllIn);
+		multiPanel.getActionMap().put(actionZoomAllIn, actionZoomAllIn);
+		multiPanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke('-'), actionZoomAllOut);
+		multiPanel.getActionMap().put(actionZoomAllOut, actionZoomAllOut);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -54,7 +83,7 @@ public class FsmDashboard extends JFrame {
 			JComboBox<String> combo = (JComboBox<String>) e.getSource();
 			int index = comboIndex(combo);
 			List<FsmData> dataList = model.data().collect(Collectors.toList());
-			view[index].setData(dataList.get(combo.getSelectedIndex()));
+			views[index].setData(dataList.get(combo.getSelectedIndex()));
 		}
 	}
 
@@ -80,20 +109,20 @@ public class FsmDashboard extends JFrame {
 			multiPanel.getComboBox(i).setModel(new FsmSelectionModel(model));
 			if (i < dataList.size()) {
 				multiPanel.getComboBox(i).setSelectedIndex(i);
-				view[i].setData(dataList.get(i));
+				views[i].setData(dataList.get(i));
 			} else {
 				multiPanel.getComboBox(i).setSelectedIndex(-1);
-				view[i].setData(null);
+				views[i].setData(null);
 			}
 		}
 	}
 
 	public void update() {
 		for (int i = 0; i < 4; ++i) {
-			if (view[i].getData() != null) {
-				view[i].getData().updateGraph();
+			if (views[i].getData() != null) {
+				views[i].getData().updateGraph();
 			}
-			view[i].update();
+			views[i].update();
 		}
 	}
 }
