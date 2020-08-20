ac9178acf589bed509f6bee191ee5a16367999f5
Armin Reichert <armin.reichert@web.de>
2020-08-20 17:26:42 +0200

    cleanup

--- PacManGame/src/main/java/de/amr/games/pacman/controller/steering/ghost/BouncingOnBed.java
+++ PacManGame/src/main/java/de/amr/games/pacman/controller/steering/ghost/BouncingOnBed.java
@@ -4,25 +4,29 @@ import static de.amr.games.pacman.model.world.api.Direction.DOWN;
 import static de.amr.games.pacman.model.world.api.Direction.UP;
 
 import de.amr.games.pacman.controller.steering.api.Steering;
-import de.amr.games.pacman.model.world.api.Tile;
 import de.amr.games.pacman.model.world.components.Bed;
 import de.amr.games.pacman.model.world.core.MovingGuy;
 
+/**
+ * Lets a guy bounce on its bed.
+ * 
+ * @author Armin Reichert
+ */
 public class BouncingOnBed implements Steering {
 
-	private final Bed bed;
+	private final float bedCenterY;
 
 	public BouncingOnBed(Bed bed) {
-		this.bed = bed;
+		bedCenterY = bed.center().y;
 	}
 
 	@Override
-	public void steer(MovingGuy entity) {
-		float dy = entity.tf.y + Tile.SIZE / 2 - bed.center().y;
+	public void steer(MovingGuy guy) {
+		float dy = guy.tf.y + guy.tf.height / 2 - bedCenterY;
 		if (dy < -4) {
-			entity.wishDir = DOWN;
+			guy.wishDir = DOWN;
 		} else if (dy > 5) {
-			entity.wishDir = UP;
+			guy.wishDir = UP;
 		}
 	}
 }
