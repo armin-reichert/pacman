b0ed68de117541988734c6b02f60e787b6aa40dd
Armin Reichert <armin.reichert@web.de>
2020-08-21 09:18:41 +0200

    change bit

--- PacManGame/src/main/java/de/amr/games/pacman/model/world/core/MapBasedWorld.java
+++ PacManGame/src/main/java/de/amr/games/pacman/model/world/core/MapBasedWorld.java
@@ -12,8 +12,7 @@ public abstract class MapBasedWorld extends AbstractWorld {
 
 	//@formatter:off
 	public static final byte B_WALL         = 0;
-	public static final byte B_1            = 1;
-	public static final byte B_INTERSECTION = 2;
+	public static final byte B_INTERSECTION = 1;
 	public static final byte B_FOOD         = 3;
 	public static final byte B_EATEN        = 4;
 	//@formatter:on
