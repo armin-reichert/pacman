1c24df4143a271d242369a12fb45aa7b7723cafe
Armin Reichert <armin.reichert@web.de>
2020-11-01 16:43:48 +0100

    cleanup

--- PacManDataOriented/src/main/java/de/amr/games/pacman/Creature.java
+++ PacManDataOriented/src/main/java/de/amr/games/pacman/Creature.java
@@ -1,17 +1,13 @@
 package de.amr.games.pacman;
 
-import static de.amr.games.pacman.World.TS;
-
 public class Creature {
 
 	public Creature(String name, V2 homeTile) {
 		this.name = name;
 		this.homeTile = homeTile;
-		this.size = new V2(TS, TS);
 	}
 
 	public final String name;
-	public final V2 size;
 	public final V2 homeTile;
 
 	public boolean visible;
