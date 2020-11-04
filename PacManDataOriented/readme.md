cd0200fa2d108542b4120561f42e030f9f9a9475
Armin Reichert <armin.reichert@web.de>
2020-11-04 16:02:32 +0100

    moved

--- PacManDataOriented/readme.md
+++
@@ -1,1 +0,0 @@
-A separate Pac-Man game implementation that uses no external libraries and contains no bells and whistles.
--- PacManDataOriented/src/main/java/de/amr/games/pacman/GameState.java
+++
@@ -1,6 +0,0 @@
-package de.amr.games.pacman;
-
-public enum GameState {
-
-	READY, SCATTERING, CHASING, CHANGING_LEVEL, PACMAN_DYING, GAME_OVER;
-}
